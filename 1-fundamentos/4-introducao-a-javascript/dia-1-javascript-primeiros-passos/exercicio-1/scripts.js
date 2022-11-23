3c9033c14b883e5e1a96775af3d836211f6d0dc1
coelhoreinaldo <scoelhor7@gmail.com>
2022-11-23 13:15:41 -0300

    introducao-a-javascript-exercicio-1

---
+++ 1-fundamentos/4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicio-1/scripts.js
@@ -0,0 +1,5 @@
+const myName = 'Reinaldo'; //não é possível alterar uma variável declarada com const, pois ela é uma constante
+const birthCity = 'Brasília';
+let birthYear = 1999;
+
+console.log(myName + ' - ' + birthCity + ', ', birthYear)
