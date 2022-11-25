c4ddb66bb49d5a96744dff6b011995bbb6c9224a
coelhoreinaldo <scoelhor7@gmail.com>
2022-11-25 13:55:44 -0300

    academia de logica 4-2

---
+++ 1-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/academia-de-logica/4-2-3.js
@@ -0,0 +1,22 @@
+let order = 3;
+let lunch;
+
+switch(order) {
+    case 1:
+        lunch = 'Trybe Lanche Feliz';
+        break;
+    case 2:
+        lunch = 'McTrybe';
+        break;
+    case 3:
+        lunch = 'TrybeWooper';
+        break;     
+    case 4:
+        lunch = 'X-Trybe';
+        break;
+    case 5:
+        lunch = 'Triplo Trybe com JS';
+        break;
+}
+
+console.log(lunch);
