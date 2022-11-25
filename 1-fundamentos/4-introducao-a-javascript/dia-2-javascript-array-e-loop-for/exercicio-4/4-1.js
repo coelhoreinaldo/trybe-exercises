b5d11c4eb8ca0a8928dea5723e847b730ee37f1e
coelhoreinaldo <scoelhor7@gmail.com>
2022-11-25 15:30:43 -0300

    4-2 dia 2

--- 1-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-4/4-1.js
+++ 1-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-4/4-1.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (i = 0; i < numbers.length; i+=1) {
+for (i = 0; i < numbers.length; i +=1 ) {
     console.log(numbers[i]);
 }
---
+++ 1-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-4/4-2.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for(i = 0; i < numbers.length; i += 1) {
+    sum = sum + numbers[i];
+}
+
+console.log(sum);
