0dc8835083545585b9bac98db2de14e73abe739c
coelhoreinaldo <scoelhor7@gmail.com>
2022-11-25 15:33:42 -0300

    4-3 dia 4

---
+++ 1-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-4/4-3.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for(i = 0; i < numbers.length; i += 1) {
+    sum = sum + numbers[i];
+}
+let media = sum / numbers.length;
+
+console.log(media);
