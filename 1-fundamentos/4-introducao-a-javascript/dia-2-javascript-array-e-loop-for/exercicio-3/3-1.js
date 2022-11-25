dec5ad43c5fcca178301ef116778e0adb7f94359
coelhoreinaldo <scoelhor7@gmail.com>
2022-11-25 08:48:16 -0300

    dia 2 3-1

---
+++ 1-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-3/3-1.js
@@ -0,0 +1,7 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let bananas of names) {
+    console.log(bananas);
+}
+
+//independente do nome que tem ali antes do of, vai imprimir certinho
