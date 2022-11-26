fc58d16e7c291c1e4613e85da845c08550a39513
coelhoreinaldo <scoelhor7@gmail.com>
2022-11-26 12:49:09 -0300

    5-1 dia 2

--- 1-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-5-ordenacao-bubble-sort/5-1.js
+++ 1-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-5-ordenacao-bubble-sort/5-1.js
@@ -4,12 +4,14 @@ Repita os dois passos acima (n - 1) vezes, onde n é igual ao tamanho do vetor*/
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let i = 0; i < numbers.length; i += 1) {
-    // console.log(i);
-    for (let j = 0; j < numbers.length - i - 1; j += 1) {
-        // console.log(j);
+for(let i = 1; i < numbers.length; i += 1) {
+    // console.log('primeiro loop', numbers[i]);
+    for (let j = 0; j < numbers.length - i; j += 1) {
+        console.log('segundo loop ', numbers[j]);
         if (numbers[j] > numbers[j + 1]){
             let sort = numbers[j];
+            // console.log('sort', sort);
+            // console.log('numbers j+1', numbers[j+1]);
             numbers[j] = numbers[j + 1];
             numbers[j + 1] = sort;
         }
