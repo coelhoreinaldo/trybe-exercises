934b662abb60123dd5959c0fa8809319dca9a84a
coelhoreinaldo <scoelhor7@gmail.com>
2022-11-26 19:09:23 -0300

    dia 2 concluído

---
+++ 1-fundamentos/4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicio-5-ordenacao-bubble-sort/5-3.js
@@ -0,0 +1,16 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sumResult = [];
+
+for (let i = 0; i < numbers.length; i += 1) {
+    console.log(numbers[i])
+    if (i + 1 < numbers.length) {
+        sumResult.push(numbers[i] * numbers[i + 1])
+        console.log('sum result if' + sumResult)
+    } else {
+        sumResult.push(numbers[i] * 2)
+        console.log('sum result else ' + sumResult)
+    }
+}
+console.log(sumResult); 
+
+//maior dificuldade: na linha 7 eu estava colocando ~ (numbers[i] + 1), ao invés de numbers[i + 1]... ):
