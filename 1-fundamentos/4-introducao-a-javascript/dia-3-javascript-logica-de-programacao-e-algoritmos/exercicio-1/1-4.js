ecadcca723df9fa0e3a43bede87dfcbf3947a7a1
coelhoreinaldo <scoelhor7@gmail.com>
2022-11-27 12:15:38 -0300

    1-4 corrigida

--- 1-fundamentos/4-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-1/1-4.js
+++ 1-fundamentos/4-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-1/1-4.js
@@ -2,7 +2,10 @@ const n = 5;
 
 let resultado = 0;
 for (let index = 0; index <= n; index += 1) {
-  resultado =+ index;
+  resultado += index;
 //   console.log(resultado);
 };
 console.log(resultado)
+
+// resultado += index
+// Essa alternativa está correta! O += indica que a cada iteração do _loop for_ a variável resultado é somada ao valor de index e passa a armazenar um novo valor.
