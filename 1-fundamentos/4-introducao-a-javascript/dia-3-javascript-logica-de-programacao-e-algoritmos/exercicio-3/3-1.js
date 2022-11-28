510ee8e0ba36ddccbed14290664baec215a20f74
coelhoreinaldo <scoelhor7@gmail.com>
2022-11-28 15:53:34 -0300

    nítida compreensão do 3-1 e do 3-2

--- 1-fundamentos/4-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-3/3-1.js
+++ 1-fundamentos/4-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-3/3-1.js
@@ -1,3 +1,5 @@
+// Escreva um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
+
 let n = 5;
 let ast = '*';
 let final = '';
