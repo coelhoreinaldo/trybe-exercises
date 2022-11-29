e89ac3d7fe274307d7766f6801b6fec0f673152d
coelhoreinaldo <scoelhor7@gmail.com>
2022-11-29 15:20:15 -0300

    3-2

---
+++ 1-fundamentos/4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-3/3-2.js
@@ -0,0 +1,33 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+let z = 10;
+let x = 2;
+
+// Adição (a + b)
+function sum (a, b) {
+    return a + b;
+}
+console.log(sum(z, x));
+
+// Subtração (a - b)
+function subtraction (a, b) {
+    return a - b;
+}
+console.log(subtraction(z, x));
+
+// Multiplicação (a * b)
+function multiplication (a, b) {
+    return a * b;
+}
+console.log(multiplication(z, x));
+
+// Divisão (a / b)
+function division (a, b) {
+    return a / b;
+}
+console.log(division(z, x));
+
+// Módulo (a % b)
+function modulus (a, b) {
+    return a % b;
+}
+console.log(modulus(z, x));
