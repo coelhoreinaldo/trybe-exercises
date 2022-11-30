e7711a0e59d22a92ed4b7b7ce1685812ebda2d47
coelhoreinaldo <scoelhor7@gmail.com>
2022-11-30 09:35:34 -0300

    4-3

---
+++ 1-fundamentos/4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-4/4-3.js
@@ -0,0 +1,11 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  info['recorrente'] = 'Sim'
+
+  for (let keys in info) {
+    console.log(keys);
+  }
