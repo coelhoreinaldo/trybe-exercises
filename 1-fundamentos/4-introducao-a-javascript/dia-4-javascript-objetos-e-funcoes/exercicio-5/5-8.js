7d80e583156adb694830e47bb02fa71bb9cf25d1
coelhoreinaldo <scoelhor7@gmail.com>
2022-11-30 19:34:44 -0300

    corrigido

--- 1-fundamentos/4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-5/5-8.js
+++ 1-fundamentos/4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-5/5-8.js
@@ -1,12 +1,12 @@
 /*Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Considere que a string ending sempre será menor que a string word. */
 
 function verificaString (stringWord, stringEnding) {
-    stringWord = stringWord.split(''); //aqui a gente tá despedaçando a palavra em letrinhas;
+    stringWord = stringWord.split(''); 
     stringEnding = stringEnding.split(''); 
-    let verifica = true;  //é o nosso verificador
-    if (stringEnding < stringWord) { //esse if é só pra tipo, obrigar o input a escrever uma segunda palavra menor que a primeira
+    let verifica = true;  
+    if (stringEnding < stringWord) {
         for (let i = 0; i < stringEnding.length; i += 1) {
-            if (stringWord[stringWord.length - stringEnding.length + i] != stringEnding[i]) { // aqui basicamente tá escrito: if t r y b e []]
+            if (stringWord[stringWord.length - stringEnding.length + i] != stringEnding[i]) { 
                 verifica = false;
             }
         }
@@ -14,8 +14,8 @@ function verificaString (stringWord, stringEnding) {
     } else {
         console.log('A segunda palavra deve ser menor que a primeira!');
     }
-}
+} 
 
 console.log(verificaString('trybe', 'be')); //Valor esperado no retorno da função: true
 console.log(verificaString('joaofernando', 'fernan')); //Retorno esperado: false
-console.log(verificaString('reinaldo', 'ldo'));
+console.log(verificaString('reinaldo', 'ldo')); 
