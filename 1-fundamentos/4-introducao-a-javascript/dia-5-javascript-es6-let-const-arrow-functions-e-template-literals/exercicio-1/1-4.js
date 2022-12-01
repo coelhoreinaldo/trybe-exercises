103c3901f85750ace098c8e735ebbae34a3fff6c
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-01 18:26:54 -0300

    1-4 template literals

---
+++ 1-fundamentos/4-introducao-a-javascript/dia-5-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-1/1-4.js
@@ -0,0 +1,12 @@
+const name = 'Adriana';
+const lastName = 'Soares';
+// console.log('Olá' + ',' + name + ' ' + lastName + '!');
+console.log(`Olá, ${name} ${lastName}!`);
+function soma(a,b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+// console.log('O resultado da soma de ' + a + ' + ' + b + ' é: ' + soma(a,b));
+console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
