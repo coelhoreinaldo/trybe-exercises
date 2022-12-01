f1b7880f7998f5dea2290539b4feae5233b44c35
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-01 10:05:21 -0300

    6-4-1 nomes e sobrenomes

---
+++ 1-fundamentos/4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-6-bonus/6-3.js
@@ -0,0 +1,45 @@
+// A partir do array de frutas basket, retorne um objeto que contenha o nome da fruta como chave e a quantidade de vezes que ela aparece no array como valor. Por exemplo, o array [‘Melancia’, ‘Abacate’, ‘Melancia’, ‘Melancia’, ‘Uva’], deverá retornar { Melancia: 3, Abacate: 1, Uva: 1 } quando passado como argumento para a função.
+
+// Em seguida, imprima esse resultado na tela com uma mensagem no seguinte formato: Sua cesta possui: x Melancias, x Abacates...
+
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+  ];
+
+let repeatedFruit = null;
+let repeats = null;
+let repeatedFruitsIndex = null;
+
+function totalFruits(array) {
+    // let divideArray = array.join(',');
+    for (let fruits in basket) {
+        let verificaFruits = basket[fruits];
+        for (let fruits2 in basket) {
+            if (basket[fruits2] === verificaFruits){  
+                repeats += 1;
+                repeatedFruit = basket[fruits2];
+                repeatedFruitsIndex = fruits2;            
+            }
+            return "Sua cesta possui: ", repeats, repeatedFruit
+        }
+    }
+    
+}
+
+console.log(totalFruits(basket));
