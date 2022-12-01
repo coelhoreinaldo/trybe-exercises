876fe1e4ff531f14987ab2ccd00af1d432fcb942
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-01 16:03:20 -0300

    1-1 var, let, escopo

---
+++ 1-fundamentos/4-introducao-a-javascript/dia-5-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-1/1-1.js
@@ -0,0 +1,12 @@
+// 🚀 Altere o tipo das variáveis (var) para let ou const para que respeitem o escopo em que foram declaradas.
+
+// 🚀 Modifique a variável para que respeite o escopo onde está sendo declarada. Lembre-se: para que a variável respeite o escopo, ela não deve ser acessível fora do escopo em que esteja sendo declarada.
+
+// Copie o código abaixo e rode-o para verificar sua saída:
+
+function imprimeIdade() {
+    for (let idade = 30; idade <= 40; idade += 1) {
+      console.log('Idade dentro do for:', idade)
+    }
+  }
+  imprimeIdade()
