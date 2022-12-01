a0c571b27beeb7971121cbd3c221396d53a8c2c4
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-01 08:45:23 -0300

    5-6 most repeated number

--- 1-fundamentos/4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-5/5-6-most-repeated-numbers.js
+++ 1-fundamentos/4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-5/5-6-most-repeated-numbers.js
@@ -1,18 +1,27 @@
-// // Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 
-// function mostRepeated(numbers) {
-//     let mostRepeatedInteger = null;
-//     let repeats = null;
-//     let indexRepeatedInteger = null;
+function mostRepeated(numbers) {
+    let contRepeated = null;
+    let contNum = null;
+    let repeatedIntegerIndex = null;
 
-//     for (let number in numbers) {
-//         if (numbers[number] === numbers[number + 1]) {
-//             mostRepeatedInteger += numbers[number];
-//             indexRepeatedInteger = number;
-//             repeats += 1
-//         }
-//     }
-//     return mostRepeatedInteger;
-// }
-    
-// console.log(mostRepeated([2, 3, 2, 5, 8, 2, 3])); // Valor esperado no retorno da função: 2.
+    for (let i in numbers) {
+        let verificaNumber = numbers[i];
+        console.log('verificaNumber ',verificaNumber);
+        for (let i2 in numbers) {
+            if (verificaNumber === numbers[i2]) {
+                contNum += 1;
+            }
+            console.log('contNum ', contNum);
+        }
+        if (contNum > contRepeated) {
+            contRepeated = contNum;
+            repeatedIntegerIndex = i;
+            console.log('contRepeated ', contRepeated);
+            console.log('repeatedIntegerIndex ', repeatedIntegerIndex);
+        }
+        contNum = 0;
+    }
+    return numbers[repeatedIntegerIndex]
+}
+console.log(mostRepeated([2, 3, 2, 5, 8, 2, 3])); // Valor esperado no retorno da função: 2.
