ebb2abd962aeaf881cd870d206e077c5c3ffba72
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-03 07:06:17 -0300

    requisito 3

---
+++ 1-fundamentos/4-introducao-a-javascript/dia-6-javascript-projeto-playground-functions/3.js
@@ -0,0 +1,5 @@
+const concatName = (object) => `${object[object.length - 1]}, ${object[0]}` 
+
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
+console.log(concatName(['foguete', 'não', 'tem', 'ré']))
+console.log(concatName(['captain', 'my', 'captain']))
