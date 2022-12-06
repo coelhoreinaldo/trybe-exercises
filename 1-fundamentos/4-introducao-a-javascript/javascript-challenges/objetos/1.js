ce283f76d51eecc85f73efc701cdf1ee3c99fc00
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-06 13:54:25 -0300

    1 objetos challenges

---
+++ 1-fundamentos/4-introducao-a-javascript/javascript-challenges/objetos/1.js
@@ -0,0 +1,32 @@
+
+
+/*
+01 - Jogo de fantasia
+Você é responsável pelo sistema de um novo jogo de fantasia medieval.
+Escreva um algoritmo que recebe o nome da classe e retorne os pontos de vida do(a) personagem, com base na tabela a seguir:
+|   Classe     |   Pontos de Vida   |
+|    Mago      |        5PV         |
+|   Arqueiro   |        10PV        |
+|   Guerreiro  |        15PV        |
+|   Cavaleiro  |        20PV        |
+Por exemplo:
+------------
+Entrada:
+"Mago"'
+Saída:
+"Mago, 5PV"
+------------
+Desafio: faça sem usar estruturas condicionais (ifs ou switches) 
+*/
+
+function fantasyGame(className) {
+    let classe = {
+        Mago: '5PV',
+        Arqueiro: '10PV',
+        Guerreiro: '15PV',
+        Cavaleiro: '20PV',
+    }
+    return `${className}, ${classe[className]}`
+  }
+
+  console.log(fantasyGame('Guerreiro'));
