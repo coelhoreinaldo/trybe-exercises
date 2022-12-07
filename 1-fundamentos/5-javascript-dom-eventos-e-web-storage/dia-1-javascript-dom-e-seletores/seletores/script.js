198fbcbd6340c10b06794f213aef9b3ce4aaa8b4
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-07 10:39:13 -0300

    seletores exercise post feedback

--- 1-fundamentos/5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/seletores/script.js
+++ 1-fundamentos/5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/seletores/script.js
@@ -1,18 +1,23 @@
-document.getElementById('header-container').style.backgroundColor = '#00B069';
-document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = '#FF9F84';
-document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = '#F9DB5E';
-document.querySelectorAll('.emergency-tasks')
+const header = document.getElementById("header-container");
+header.style.backgroundColor = "#00B069";
 
-let emergencyTasks = document.querySelectorAll('.emergency-tasks h3');
+const emergencyTasksHeader =
+  document.getElementsByClassName("emergency-tasks")[0];
+emergencyTasksHeader.style.backgroundColor = "#FF9F84";
 
+const noEmergencyTasksHeader =
+  document.getElementsByClassName("no-emergency-tasks")[0];
+noEmergencyTasksHeader.style.backgroundColor = "#F9DB5E";
+
+let emergencyTasks = document.querySelectorAll(".emergency-tasks h3");
 for (let i = 0; i < emergencyTasks.length; i += 1) {
-    emergencyTasks[i].style.backgroundColor = '#A500F3';
+  emergencyTasks[i].style.backgroundColor = "#A500F3";
 }
 
-let noEmergencyTasks = document.querySelectorAll('.no-emergency-tasks h3');
-
+let noEmergencyTasks = document.querySelectorAll(".no-emergency-tasks h3");
 for (let i = 0; i < emergencyTasks.length; i += 1) {
-    noEmergencyTasks[i].style.backgroundColor = 'black';
+  noEmergencyTasks[i].style.backgroundColor = "black";
 }
 
-document.querySelector('#footer-container').style.backgroundColor = '#003533';
+const footer = document.querySelector("#footer-container");
+footer.style.backgroundColor = "#003533";
