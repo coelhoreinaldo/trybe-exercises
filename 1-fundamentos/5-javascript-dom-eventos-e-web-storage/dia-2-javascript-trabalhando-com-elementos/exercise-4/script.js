2e994a8b3aa9b9d72d0623351a64de40b28d60ac
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-08 18:28:15 -0300

    exercise 4 requisito 2

--- 1-fundamentos/5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercise-4/script.js
+++ 1-fundamentos/5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercise-4/script.js
@@ -1,10 +1,13 @@
  //   Adicione a tag h1 com o texto TrybeTrip - Agência de Viagens como filho da tag body;
 const fatherBody = document.querySelector('body');
-const h1Text = document.createElement('h1');
-h1Text.id = 'h1text'
-fatherBody.appendChild(h1Text)
-h1Text.innerText = 'TrybeTrip - Agência de Viagens'
+const createH1Text = document.createElement('h1');
+createH1Text.id = 'h1text'
+fatherBody.appendChild(createH1Text)
+createH1Text.innerText = 'TrybeTrip - Agência de Viagens'
 
 // Adicione a tag main com a classe main-content como filho da tag body;
+const createMain = document.createElement('main');
+createMain.className = 'main-content';
+fatherBody.appendChild(createMain)
 // Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
 // Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
