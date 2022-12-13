7200e0acad4fa786d6d36e8523372c9ae8b777b7
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-13 15:16:46 -0300

    exercise 5 part 1 change background color refactored, :)

--- 1-fundamentos/5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercise-5/script.js
+++ 1-fundamentos/5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercise-5/script.js
@@ -1,42 +1,50 @@
 
 window.onload = () => {
     const body = document.body;
-    const whiteButton = document.querySelectorAll(".white-button");
-    const blackButton = document.querySelectorAll(".black-button");
-    const greenButton = document.querySelectorAll(".green-button");
-    const blueButton = document.querySelectorAll(".blue-button");
-    const yellowButton = document.querySelectorAll(".yellow-button");
+    // const whiteButton = document.querySelectorAll(".white-button");
+    // const blackButton = document.querySelectorAll(".black-button");
+    // const greenButton = document.querySelectorAll(".green-button");
+    // const blueButton = document.querySelectorAll(".blue-button");
+    // const yellowButton = document.querySelectorAll(".yellow-button");
 
 const changeBackgroundColor = () => {
     localStorage.getItem('body.style.backgroundColor')
-    let color;
-    whiteButton[0].addEventListener('click', () => {
-        color = whiteButton[0].innerHTML;        
-        body.style.backgroundColor = color;
-        localStorage.setItem('body.style.backgroundColor', 'color');
-    })
-    blackButton[0].addEventListener('click', () => {
-        color = blackButton[0].innerHTML;     
-        body.style.backgroundColor = color;
-    })
-    greenButton[0].addEventListener('click', () => {
-        color = greenButton[0].innerHTML;     
-        body.style.backgroundColor = color;
-    })
-    blueButton[0].addEventListener('click', () => {
-        color = blueButton[0].innerHTML;     
-        body.style.backgroundColor = color;
-    })
-    yellowButton[0].addEventListener('click', () => {
-        color = yellowButton[0].innerHTML;     
-        body.style.backgroundColor = color;
-    })
-    localStorage.setItem(body.style.backgroundColor, color); // Minha dúvida: como eu salvo minhas coisas no local storage po?
+    let color = '';
+    let backgroundColorButtons = document.querySelectorAll('.my-buttons')[0].children;
+    for (let i = 0; i < backgroundColorButtons.length; i += 1){
+        backgroundColorButtons[i].addEventListener('click', () => {
+            color = backgroundColorButtons[i].innerHTML;
+            body.style.backgroundColor = color;
+        })
+    }
+    // whiteButton[0].addEventListener('click', () => {
+    //     color = whiteButton[0].innerHTML;        
+    //     body.style.backgroundColor = color;
+    //     localStorage.setItem('body.style.backgroundColor', 'color');
+    // })
+    // blackButton[0].addEventListener('click', () => {
+    //     color = blackButton[0].innerHTML;     
+    //     body.style.backgroundColor = color;
+    // })
+    // greenButton[0].addEventListener('click', () => {
+    //     color = greenButton[0].innerHTML;     
+    //     body.style.backgroundColor = color;
+    // })
+    // blueButton[0].addEventListener('click', () => {
+    //     color = blueButton[0].innerHTML;     
+    //     body.style.backgroundColor = color;
+    // })
+    // yellowButton[0].addEventListener('click', () => {
+    //     color = yellowButton[0].innerHTML;     
+    //     body.style.backgroundColor = color;
+    // })
+    // localStorage.setItem(body.style.backgroundColor, color); // Minha dúvida: como eu salvo minhas coisas no local storage po?
 }
 
 changeBackgroundColor();
 
 const changeFontColor = () => {
+    let color = '';
     let fontColorButtons = document.querySelectorAll('.my-buttons')[1].children;
     for (let i = 0; i < fontColorButtons.length; i += 1) {
         fontColorButtons[i].addEventListener('click', () => {
