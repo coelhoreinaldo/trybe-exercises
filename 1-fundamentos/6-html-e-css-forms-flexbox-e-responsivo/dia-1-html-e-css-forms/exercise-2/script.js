b8b725655820dd5a4bb7713439640fdd3aabd3d0
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-21 17:35:40 -0300

    exercise 3 TrybeTrip Iniciando Forms, Section 1 e 2

--- 1-fundamentos/6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/exercise-2/script.js
+++ 1-fundamentos/6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/exercise-2/script.js
@@ -18,9 +18,5 @@ INPUT_TEXT.addEventListener('keypress', (event) => {
   }
 });
 
-// Adicione um evento click no elemento a que possui o id href. Na função do evento, implemente o método .preventDefault() para prevenir o comportamento padrão do elemento. Ou seja, ao clicar no link, nada deve acontecer.
 
-// Adicione um evento click no elemento checkbox que possui o id input-checkbox. Na função do evento, implemente o método .preventDefault() para prevenir o comportamento padrão do elemento. Ou seja, ao clicar na caixinha, nada deve acontecer.
-
-// Adicione um evento keypress no elemento input type="text" que possui o id input-text. Na função do evento, implemente o método .preventDefault() de forma que somente o caractere a (letra ‘a’ minúscula) possa ser digitado na caixinha. (dica: para capturar a tecla digitada, utilize o event.key)
 
