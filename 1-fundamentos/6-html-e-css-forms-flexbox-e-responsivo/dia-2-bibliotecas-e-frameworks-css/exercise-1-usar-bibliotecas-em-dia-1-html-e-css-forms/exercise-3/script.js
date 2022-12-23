3f4ad1b02edf09c921cbfed0be2816bd8e922a24
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-23 09:30:56 -0300

    iniciando dia 2

---
+++ 1-fundamentos/6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-e-frameworks-css/exercise-1-usar-bibliotecas-em-dia-1-html-e-css-forms/exercise-3/script.js
@@ -0,0 +1,38 @@
+const submitBtn = document.getElementById('submit-btn');
+const imagesAgreement = document.getElementById('images');
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  const validation = textInputValidation();
+  if (validation === false) {
+    alert('Dados inválidos');
+  } else {
+    alert ('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+  }
+});
+
+const enableSubmit = () => {
+  submitBtn.disabled = !imagesAgreement.checked;
+};
+
+const textInputValidation = () => {
+  const nameInput = document.getElementById('user-name').value.length;
+  const invalidName = nameInput < 10 || nameInput > 40;
+
+  const emailInput = document.getElementById('user-email').value.length;
+  const invalidEmail = emailInput < 10 || emailInput > 40;
+
+  const whyMe = document.getElementById('why-me').value.length;
+  const invalidWhyMe = whyMe > 500;
+
+  if (invalidEmail || invalidName || invalidWhyMe) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+window.onload = () => {
+  imagesAgreement.addEventListener('change', enableSubmit);
+};
