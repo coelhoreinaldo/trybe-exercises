e174bf0a64a27dfbefa6a6c909203bb8ded85fbe
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-28 11:01:16 -0300

    validations

--- 1-fundamentos/6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-e-frameworks-css/exercise-1-usar-bibliotecas-em-dia-1-html-e-css-forms/exercise-3/script.js
+++ 1-fundamentos/6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-e-frameworks-css/exercise-1-usar-bibliotecas-em-dia-1-html-e-css-forms/exercise-3/script.js
@@ -1,5 +1,79 @@
-// const submitBtn = document.getElementById('submit-btn');
-// const imagesAgreement = document.getElementById('images');
+const picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  format: 'D/M/YYYY',
+  toString(date, format) {
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+      const parts = dateString.split('/');
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10) - 1;
+      const year = parseInt(parts[2], 10);
+      return new Date(year, month, day);
+  }
+}); //perguntar na aula
+
+const validation = new JustValidate('#form', {
+  errorFieldCssClass: 'is-invalid',
+  errorFieldStyle: {
+    border: '6px solid red',
+  },
+  errorLabelCssClass: 'is-label-invalid',
+  errorLabelStyle: {
+    color: 'red',
+    textDecoration: 'underlined',
+  },
+  focusInvalidField: true,
+  lockForm: true,
+  tooltip: {
+    position: 'top',
+  },
+});
+
+validation.addField("#user-name", [
+  {
+    rule: "minLength",
+    value: 10,
+  },
+  {
+    rule: "maxLength",
+    value: 40,
+  },
+  {
+    rule: "required",
+    errorMessage: "Preencha com seu nome",
+  },
+]);
+validation.addField("#user-email", [
+  {
+    rule: "minLength",
+    value: 10,
+  },
+  {
+    rule: "maxLength",
+    value: 50,
+  },
+  {
+    rule: "required",
+    errorMessage: "Email é necessário",
+  },
+  {
+    rule: "email",
+    errorMessage: "Formato de email inválido"
+  },
+]);
+validation.addField("#why-me", [
+  {
+    rule: "maxLength",
+    value: 500,
+  }
+]);
+
+const submitBtn = document.getElementById('submit-btn');
+const imagesAgreement = document.getElementById('images');
 
 // submitBtn.addEventListener('click', (event) => {
 //   event.preventDefault()
@@ -12,29 +86,11 @@
 //   }
 // });
 
-// const enableSubmit = () => {
-//   submitBtn.disabled = !imagesAgreement.checked;
-// };
-
-// const textInputValidation = () => {
-//   const nameInput = document.getElementById('user-name').value.length;
-//   const invalidName = nameInput < 10 || nameInput > 40;
-
-//   const emailInput = document.getElementById('user-email').value.length;
-//   const invalidEmail = emailInput < 10 || emailInput > 40;
-
-//   const whyMe = document.getElementById('why-me').value.length;
-//   const invalidWhyMe = whyMe > 500;
-
-//   if (invalidEmail || invalidName || invalidWhyMe) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// };
-
-// window.onload = () => {
-//   imagesAgreement.addEventListener('change', enableSubmit);
-// };
+const enableSubmit = () => {
+  submitBtn.disabled = !imagesAgreement.checked;
+};
 
 
+window.onload = () => {
+  imagesAgreement.addEventListener('change', enableSubmit);
+};
