71ce27a53016a016a55cbd4e48adde0799fe23cf
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-27 21:17:03 -0300

    just validate

--- 1-fundamentos/6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-e-frameworks-css/exercise-1-usar-bibliotecas-em-dia-1-html-e-css-forms/exercise-3/script.js
+++ 1-fundamentos/6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-e-frameworks-css/exercise-1-usar-bibliotecas-em-dia-1-html-e-css-forms/exercise-3/script.js
@@ -1,38 +1,40 @@
-const submitBtn = document.getElementById('submit-btn');
-const imagesAgreement = document.getElementById('images');
+// const submitBtn = document.getElementById('submit-btn');
+// const imagesAgreement = document.getElementById('images');
 
-submitBtn.addEventListener('click', (event) => {
-  event.preventDefault()
+// submitBtn.addEventListener('click', (event) => {
+//   event.preventDefault()
 
-  const validation = textInputValidation();
-  if (validation === false) {
-    alert('Dados inválidos');
-  } else {
-    alert ('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
-  }
-});
+//   const validation = textInputValidation();
+//   if (validation === false) {
+//     alert('Dados inválidos');
+//   } else {
+//     alert ('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+//   }
+// });
 
-const enableSubmit = () => {
-  submitBtn.disabled = !imagesAgreement.checked;
-};
+// const enableSubmit = () => {
+//   submitBtn.disabled = !imagesAgreement.checked;
+// };
 
-const textInputValidation = () => {
-  const nameInput = document.getElementById('user-name').value.length;
-  const invalidName = nameInput < 10 || nameInput > 40;
+// const textInputValidation = () => {
+//   const nameInput = document.getElementById('user-name').value.length;
+//   const invalidName = nameInput < 10 || nameInput > 40;
 
-  const emailInput = document.getElementById('user-email').value.length;
-  const invalidEmail = emailInput < 10 || emailInput > 40;
+//   const emailInput = document.getElementById('user-email').value.length;
+//   const invalidEmail = emailInput < 10 || emailInput > 40;
 
-  const whyMe = document.getElementById('why-me').value.length;
-  const invalidWhyMe = whyMe > 500;
+//   const whyMe = document.getElementById('why-me').value.length;
+//   const invalidWhyMe = whyMe > 500;
 
-  if (invalidEmail || invalidName || invalidWhyMe) {
-    return false;
-  } else {
-    return true;
-  }
-};
+//   if (invalidEmail || invalidName || invalidWhyMe) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// };
 
-window.onload = () => {
-  imagesAgreement.addEventListener('change', enableSubmit);
-};
+// window.onload = () => {
+//   imagesAgreement.addEventListener('change', enableSubmit);
+// };
+
+
