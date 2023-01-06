ecc6ff4e1c449de2b09277a4d04c27942ef513c4
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-06 12:23:59 -0300

    7-1 exercise 2

--- 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-fluxo-de-excecoes-e-manipulacao-de-objetos/exercise-2/script.js
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-fluxo-de-excecoes-e-manipulacao-de-objetos/exercise-2/script.js
@@ -16,9 +16,18 @@ const student2 = {
 const printSkills = (object) => {
   const keys = Object.keys(object);
   const values = Object.values(object);
-  for (const i in keys) {
+  for (let i = 0; i < keys.length; i += 1) {
     console.log(`Nome da habilidade: ${keys[i]}, Nível: ${values[i]}`);
   }
 } 
 
+// const printSkills = (object) => {
+//   const keys = Object.keys(object);
+//   const values = Object.values(object);
+//   for (const i in keys) {
+//     console.log(`Nome da habilidade: ${keys[i]}, Nível: ${values[i]}`);
+//   }
+// }  outra alternativa
+
 printSkills(student2);
+
