8960e2faaea5ca31cc42cd31e2b6d7131f17a7ba
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-06 16:28:59 -0300

    example 6

---
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-fluxo-de-excecoes-e-manipulacao-de-objetos/example-6-object-assign.js
@@ -0,0 +1,41 @@
+// const person = {
+//   name: 'Roberto',
+// };
+
+// const lastName = {
+//   lastName: 'Silva',
+// };
+
+// const clone = Object.assign(person, lastName);
+
+// // console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+// // console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+
+// clone.name = 'Maria';
+
+// console.log('Mudando a propriedade name através do objeto clone')
+// console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
+// console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
+// console.log('--------------');
+
+// person.lastName = 'Ferreira';
+
+// console.log('Mudando a propriedade lastName através do objeto person');
+// console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
+// console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' }
+
+const obj = { value1: 10, value2: 11 };
+const cloneObj = obj;
+
+const person = {
+  name:'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const newPerson = Object.assign({},person,lastName);
+newPerson.name = 'Gilberto';
+console.log(newPerson);
+console.log(person);
