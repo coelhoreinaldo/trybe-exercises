dfbc868614ee941972e098357add9dfb4f232ba1
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-08 12:27:30 -0300

    7-1 3.1

---
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-fluxo-de-excecoes-e-manipulacao-de-objetos/exercise-3/script.js
@@ -0,0 +1,27 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addPropriety = (object, key, value) => {
+  object[key] = value;
+};
+
+addPropriety(lesson2, 'turno', 'noite');
+
+console.log(lesson2);
