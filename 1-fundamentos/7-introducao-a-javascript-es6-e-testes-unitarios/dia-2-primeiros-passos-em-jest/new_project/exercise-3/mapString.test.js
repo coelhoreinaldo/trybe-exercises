a6958ad1de473f3f0ded661e54b170eefd6090f1
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-11 11:52:45 -0300

    7-2 ex3.2

--- 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-primeiros-passos-em-jest/new_project/exercise-3/mapString.test.js
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-primeiros-passos-em-jest/new_project/exercise-3/mapString.test.js
@@ -2,6 +2,11 @@ const { mapString, encode, decode } = require('./mapString');
 
 describe ('as funções encode, decode e mapString', () => {
   it ('a função encode e decode devem ser funções', () => {
-    expect(typeof encode).toBe('function')
-  })
+    expect(typeof encode).toBe('function');
+    expect(typeof decode).toBe('function');
+  });
+  it ('a função encode e decode deve funcionar corretamente', () => {
+    expect(encode('a, e, i, o, u')).toBe('1, 2, 3, 4, 5');
+    expect(decode('1, 2, 3, 4, 5')).toBe('a, e, i, o, u');
+  });
 });
