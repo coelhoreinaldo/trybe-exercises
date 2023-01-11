fa9b3742f28585a0bc901402fadf8c99285d0607
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-11 10:45:56 -0300

    7-2 ex 1.3

--- 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-primeiros-passos-em-jest/new_project/exercise-1/myRemove.test.js
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-primeiros-passos-em-jest/new_project/exercise-1/myRemove.test.js
@@ -1,5 +1,10 @@
 const {myRemove} = require('./myRemove');
 
-test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
-  expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
-})
+describe('função myRemove', () => {
+  it ('deve retornar o array removendo o item correto', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  })
+});
+
