a57e73dc9252397f60776cdecd59dd3862086581
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-11 11:30:58 -0300

    7-2 ex2.3

--- 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-primeiros-passos-em-jest/new_project/exercise-2/myFizzBuzz.test.js
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-primeiros-passos-em-jest/new_project/exercise-2/myFizzBuzz.test.js
@@ -4,5 +4,6 @@ describe('a função myFizzBuzz', () => {
   it ('deve retornar o resultado correto', () => {
     expect(myFizzBuzz(15)).toBe('fizzbuzz');
     expect(myFizzBuzz(3)).toBe('fizz');
+    expect(myFizzBuzz(5)).toBe('buzz');
   })
 });
