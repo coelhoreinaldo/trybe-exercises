5849f9636b920bb78b7f5564d2c428a9f033eff3
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-12 15:12:31 -0300

    7-3 ex-1.4

--- 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercise-1/createItem.test.js
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercise-1/createItem.test.js
@@ -13,7 +13,9 @@ describe('a função createItem', () => {
     expect(() => createItem()).toThrow();
     expect(() => createItem()).toThrow(Error);
   });
-  it.todo('Lança um erro se o nome do item não é uma string');
+  it ('Lança um erro se o nome do item não é uma string', () => {
+    expect(() => createItem(1.99, 'kg', 'banana', 20)).toThrow('O nome do item deve ser uma string');
+  });
   it.todo('Lança um erro se o preço é negativo');
   it.todo('Lança um erro se o preço é zero');
 });
