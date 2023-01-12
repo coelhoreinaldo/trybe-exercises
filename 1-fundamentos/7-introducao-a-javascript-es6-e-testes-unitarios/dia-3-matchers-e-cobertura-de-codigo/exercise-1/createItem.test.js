9ac2c959b27b5ee24b8edf21d6ceee2dc981eeec
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-12 15:01:30 -0300

    7-3 ex-1.1

---
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercise-1/createItem.test.js
@@ -0,0 +1,14 @@
+const createItem = require('./createItem');
+describe('a função createItem', () => {
+  it ('cria um item válido', () => {
+    expect(createItem('banana', 'kg', 1.99, 20)).toHaveProperty('name', 'banana');
+    expect(createItem('banana', 'kg', 1.99, 20)).toHaveProperty('quantity', 20);
+    expect(createItem('banana', 'kg', 1.99, 20)).toHaveProperty('unit', 'kg');
+    expect(createItem('banana', 'kg', 1.99, 20)).toHaveProperty('price', 1.99);
+  });
+  it.todo('utiliza zero como quantidade padrão');
+  it.todo('Lança um erro quando não recebe parâmetros');
+  it.todo('Lança um erro se o nome do item não é uma string');
+  it.todo('Lança um erro se o preço é negativo');
+  it.todo('Lança um erro se o preço é zero');
+});
