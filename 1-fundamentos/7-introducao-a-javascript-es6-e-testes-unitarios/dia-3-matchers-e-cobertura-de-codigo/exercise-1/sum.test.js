798ec8a93f23955f2409233f7eb06374a4425008
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-13 09:23:45 -0300

    7-3 ex1.1

--- 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercise-1/sum.test.js
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercise-1/sum.test.js
@@ -1,7 +1,9 @@
 const sum = require('./sum');
 
 describe('sum function', () => {
-  it.todo('should return 9 when sum(4, 5)')
+  it ('should return 9 when sum(4, 5)', () =>{
+    expect(sum(4, 5)).toBe(9);
+  })
   it.todo('should return 0 when sum(0, 0)')
   it.todo('should throw a error when sum(4, "5")')
   it.todo('should throw a error message "parameters must be numbers" when sum(4, "5")')
