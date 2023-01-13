c27b21e9a43907342ba141fa0ba9ae4ee16f58a5
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-13 09:25:49 -0300

    7-3 ex1.3

--- 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercise-1/sum.test.js
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercise-1/sum.test.js
@@ -7,6 +7,8 @@ describe('sum function', () => {
   it ('should return 0 when sum(0, 0)', () => {
     expect(sum(0,0)).toBe(0);
   });
-  it.todo('should throw a error when sum(4, "5")')
+  it ('should throw a error when sum(4, "5")', () => {
+    expect(() => sum(4, '5')).toThrow(Error);
+  });
   it.todo('should throw a error message "parameters must be numbers" when sum(4, "5")')
 })
