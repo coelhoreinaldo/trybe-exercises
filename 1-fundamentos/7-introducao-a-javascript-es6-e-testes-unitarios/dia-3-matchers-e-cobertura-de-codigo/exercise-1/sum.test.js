c50cb3d1c246f640a80bd5fbd049e700d79c6293
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-13 09:31:33 -0300

    7-3 ex1.4 post-feedback

--- 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercise-1/sum.test.js
+++ 1-fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercise-1/sum.test.js
@@ -1,16 +1,12 @@
 const sum = require('./sum');
 
 describe('sum function', () => {
-  it ('should return 9 when sum(4, 5)', () =>{
+  it ('should return the sum result', () =>{
     expect(sum(4, 5)).toBe(9);
-  });
-  it ('should return 0 when sum(0, 0)', () => {
-    expect(sum(0,0)).toBe(0);
+    expect(sum(0, 0)).toBe(0);
   });
   it ('should throw a error when sum(4, "5")', () => {
     expect(() => sum(4, '5')).toThrow(Error);
-  });
-  it ('should throw a error message "parameters must be numbers" when sum(4, "5")', () => {
-    expect(() => sum(4,'5')).toThrow('parameters must be numbers')
+    expect(() => sum(4, '5')).toThrow('parameters must be numbers')
   });
 });
