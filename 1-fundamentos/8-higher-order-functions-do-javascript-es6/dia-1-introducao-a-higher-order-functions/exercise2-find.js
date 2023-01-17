caa16c4405ae1dd920057bd8594033d5fd13f5a8
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-17 10:43:38 -0300

    8-1 exercise 2 find

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/exercise2-find.js
@@ -0,0 +1,6 @@
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const verifyNumbers = numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+
+console.log(verifyNumbers);
+// Adicione seu código aqui
