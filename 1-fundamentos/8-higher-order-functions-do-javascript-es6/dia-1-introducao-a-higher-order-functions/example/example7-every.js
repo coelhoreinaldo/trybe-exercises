759b9fe5e1a5b46ae07065b6ca9287361c774735
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-17 11:16:29 -0300

    8-1 example 7 every

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/example/example7-every.js
@@ -0,0 +1,9 @@
+const grades = {
+  portugues: 'Aprovado',
+  matematica: 'Reprovado',
+  ingles: 'Aprovado',
+};
+
+const verifyGrades = Object.values(grades).every((grade) => grade === 'Aprovado'); // false
+
+console.log(verifyGrades);
