60fe2251401610d3aebf24ec05d8778171ba465c
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-17 10:29:17 -0300

    8-1 ex1

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/exercise1.js
@@ -0,0 +1,12 @@
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+emailListInData.forEach((email) => {
+  console.log(`O email ${email} está cadastrado em nosso banco de dados!`);
+})
+
+// Adicione seu código aqui
