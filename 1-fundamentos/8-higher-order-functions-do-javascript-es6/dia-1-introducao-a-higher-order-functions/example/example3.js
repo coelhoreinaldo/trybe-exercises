c68e119d8d0f9ddf95c7d040a583fefb3b3a2fd2
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-17 10:21:26 -0300

    8-1 example 3 forEach * 2

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/example/example3.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/example/example3.js
@@ -1,5 +1,9 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-numbers.forEach((element) => {
-  console.log(element * 2); // [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
-});
+// numbers.forEach((element) => console.log(element * 2));
+
+const multiply = (element) => {
+  console.log(element * 2);
+}
+
+numbers.forEach(multiply);
