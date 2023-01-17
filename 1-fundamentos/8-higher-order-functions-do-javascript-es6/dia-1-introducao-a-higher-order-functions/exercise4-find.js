f93ec75feae5cb890d5712077599bce58ba81f9e
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-17 11:05:47 -0300

    8-1 exercise 4 post-feedback

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/exercise4-find.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/exercise4-find.js
@@ -4,6 +4,6 @@ const musicas = [
   { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
 ];
 
-const findSong = musicas.find((song) => song.id === '31031685'? song.title : undefined);
+const findSong = musicas.find((song) => song.id === '31031685');
 
 console.log(findSong);
