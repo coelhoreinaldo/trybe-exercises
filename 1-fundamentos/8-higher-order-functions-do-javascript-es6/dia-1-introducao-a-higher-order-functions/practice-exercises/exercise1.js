6760d44b6ff0354e71247177c0beb250ab243988
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-19 11:00:35 -0300

    8-1 ex1.logic

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/practice-exercises/exercise1.js
@@ -0,0 +1,16 @@
+const newEmployees = () => {
+  const employees = {
+    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+const employeeGenerator = (fullName) => {
+  const email = fullName.toLowerCase().replace(' ', '_');
+
+  return {fullName, email: `${email}@trybe.com`};
+}
+
+console.log(employeeGenerator('Reinaldo Coelho'))
