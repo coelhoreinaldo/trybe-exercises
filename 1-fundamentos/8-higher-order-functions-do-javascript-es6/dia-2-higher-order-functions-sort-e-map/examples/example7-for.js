c5882d18b02b51a422a7a0d1fce179fc17afad9a
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-24 17:00:09 -0300

    8-2 example7-for

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-2-higher-order-functions-sort-e-map/examples/example7-for.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = [];
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > 0) {
+    negativeNumbers.push(numbers[index] * -1);
+  } else {
+    negativeNumbers.push(numbers[index]);
+  }
+}
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
