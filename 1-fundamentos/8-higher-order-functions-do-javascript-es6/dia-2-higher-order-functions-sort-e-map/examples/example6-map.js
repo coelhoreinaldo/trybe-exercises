fd80213b9742c0f0e03c22b4120376efe90c4dbf
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-24 16:59:41 -0300

    8-2 example6-map

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-2-higher-order-functions-sort-e-map/examples/example6-map.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
