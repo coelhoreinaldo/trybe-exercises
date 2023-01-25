df2d766067a4942b9aaa9d5f96ed325491c3460f
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-25 11:24:29 -0300

    8-3 example1-filter not strings

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/examples/example1-filter.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/examples/example1-filter.js
@@ -1,5 +1,6 @@
 const dados = ['Luca', 91234567, 'Ana', 92345678, 'Marlete', 93456789];
 
-const dadosNames = dados.filter((item) => typeof item === 'string');
+const dadosStrings = dados.filter((item) => typeof item === 'string');
+const dadosNotStrings = dados.filter((item) => typeof item !== 'string');
 
-console.log(dadosNames);
+console.log(dadosNotStrings);
