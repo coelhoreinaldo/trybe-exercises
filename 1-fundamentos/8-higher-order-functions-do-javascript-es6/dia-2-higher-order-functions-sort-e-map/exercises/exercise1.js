f01e4d515d9db82b1b8c060c7942933b7996cf6f
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-25 10:00:43 -0300

    feat: 8-2 exercise2-map

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-2-higher-order-functions-sort-e-map/exercises/exercise1.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-2-higher-order-functions-sort-e-map/exercises/exercise1.js
@@ -63,15 +63,10 @@ const books = [
 
 // Adicione o código do exercício aqui:
 
-const expectedResult = [
-  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-  'Fundação - Ficção Científica - Isaac Asimov',
-  'Duna - Ficção Científica - Frank Herbert',
-  'A Coisa - Terror - Stephen King',
-  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
-];
-
 const formatedBookNames = (object) => object.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
 
-console.log(formatedBookNames(books));
+formatedBookNames(books);
+
+const formatedAuthorNamesBirth = (object) => object.map((book) => `${book.author.name} - ${book.author.birthYear}`);
+
+formatedAuthorNamesBirth(books);
