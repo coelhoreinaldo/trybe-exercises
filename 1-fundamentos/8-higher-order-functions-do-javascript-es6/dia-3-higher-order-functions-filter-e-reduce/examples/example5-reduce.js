5a842d56af2c149e96f0ed030585d172f188487e
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-25 12:11:50 -0300

    8-3 example5 reduce post-feedback

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/examples/example5-reduce.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/examples/example5-reduce.js
@@ -1,13 +1,5 @@
 const numbers = [50, 85, -30, 3, 15];
 
-const reduced = numbers.reduce((bigger, number) => {
-  let biggestNumber = null;
-  if (bigger > number){
-    biggestNumber = bigger
-  } else {
-    biggestNumber = number
-  }
-  return biggestNumber
-})
+const reduced = numbers.reduce((bigger, number) => bigger > number ? bigger : number)
 
 console.log(reduced);
