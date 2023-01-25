0df1658c115f9d7b78dcf02c4f095021fc7a3b08
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-25 11:28:14 -0300

    8-3 example1-filter > 20

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/examples/example2-filter.js
@@ -0,0 +1,6 @@
+const listaNumeros = [10, 20, 30, 40];
+
+// Armazena o resultado em uma nova variável maiorVinte
+let maiorVinte = listaNumeros.filter((item) => item > 20);
+
+console.log(maiorVinte);
