35a224667b44435060f4e953f7fbf8b453ac7552
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-25 12:19:44 -0300

    8-3 example6 reduce post-feedback

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/examples/example6-reduce.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/examples/example6-reduce.js
@@ -1,7 +1,17 @@
+// const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+// const sum = numbers.reduce((acc, curr) => {
+//   return curr % 2 === 0 ? acc + curr : acc;
+// }, 0)
+
+// console.log(sum);
+
 const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
-const sum = numbers.reduce((acc, curr) => {
-  return curr % 2 === 0 ? acc + curr : acc;
-}, 0)
+const sum = (accumulator, number) => (
+  (number % 2 === 0) ? accumulator + number : accumulator
+);
 
-console.log(sum);
+const sumNumbers = (array) => array.reduce(sum, 0);
+
+console.log(sumNumbers(numbers)); // 152
