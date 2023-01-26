9aff09d360f7dfb0cb7554d1bbf9dac728d7b3c3
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-26 16:17:06 -0300

    feat: 8-3 bonus-exercise 4 reduce

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/bonus/bonus-exercise2.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/bonus/bonus-exercise2.js
@@ -20,3 +20,20 @@ const averageAge = () => {
 }
 
 averageAge();
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+const longestNamedBook = () => {
+   return data.books.reduce((acc, curr) => acc.name.length > curr.name.length ? acc : curr);
+}
+
+console.log(longestNamedBook());
