e5b0a224eb46488aa9ff07dd293a732f33cf2926
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-26 11:27:38 -0300

    feat: 8-3 exercise1-2 filter n sort

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/exercises/exercise1.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/exercises/exercise1.js
@@ -1,38 +1,18 @@
 const data = require('../data');
 
-const expectedResult = [
-  {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: { name: 'George R. R. Martin', birthYear: 1948 },
-    releaseYear: 1991
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951
-  },
-  {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: { name: 'Frank Herbert', birthYear: 1920 },
-    releaseYear: 1965
-  }
-];
 
-const fantasyOrScienceFiction = () => {
-  return data.books.filter((element) => element.genre === 'Fantasia' || element.genre === 'Ficção Científica');
+const fantasyOrScienceFiction = () => data.books
+.filter((element) => element.genre === 'Fantasia' || element.genre === 'Ficção Científica');
+
+fantasyOrScienceFiction(); //Livros de fantasia e ficção científica
+
+const currDate = new Date();
+const currYear = currDate.getFullYear();
+
+const oldBooksOrdered = () => {
+  const filtered = data.books.filter((book) => currYear - book.releaseYear > 60);
+  const sorted = filtered.sort((a, b) => a.releaseYear - b.releaseYear)
+  return sorted
 }
 
-console.log(fantasyOrScienceFiction())
+console.log(oldBooksOrdered()); //Livros com mais de 60 anos ordenados do mais velho pro mais novo
