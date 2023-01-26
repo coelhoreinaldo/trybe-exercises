85f6f34c8f1e8776ec8b9e9c51f43ddca548ee57
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-26 14:34:15 -0300

    feat: 8-3 exercise2-2 reduce n map

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/exercises/exercise2.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/exercises/exercise2.js
@@ -1,10 +1,17 @@
 const data = require('../data');
 
-const expectedResult = 120797034;
-
 const getPopulation = () => {
   const maped = data.countries.map((element) => element.population)
   return maped.reduce((acc, curr) => acc + curr, 0);
-}
+};
 
-console.log(getPopulation());
+getPopulation();
+
+const expectedResult = 4311757;
+
+const getTotalArea = () => {
+  const maped = data.countries.map((element) => element.area)
+  return maped.reduce((acc, curr) => acc + curr, 0);
+};
+
+console.log(getTotalArea());
