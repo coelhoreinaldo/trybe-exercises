e6ef11ace68df01730bdc01075b37e0deea0367b
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-26 14:37:59 -0300

    feat: 8-3 exercise2-3 reduce

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/exercises/exercise2.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/exercises/exercise2.js
@@ -7,11 +7,25 @@ const getPopulation = () => {
 
 getPopulation();
 
-const expectedResult = 4311757;
-
 const getTotalArea = () => {
   const maped = data.countries.map((element) => element.area)
   return maped.reduce((acc, curr) => acc + curr, 0);
 };
 
-console.log(getTotalArea());
+getTotalArea();
+
+const expectedResult = {
+  name: 'American Samoa',
+  region: 'Oceania',
+  currencies: [{ code: 'USD', name: 'United States Dollar' }],
+  capital: 'Pago Pago',
+  population: 55197,
+  area: 199
+}
+
+const longestName = () => {
+  const maped = data.countries.map((element) => element.name)
+  return data.countries.reduce((acc, curr) => acc.name.length > curr.name.length ? acc : curr);
+}
+
+console.log(longestName()); 
