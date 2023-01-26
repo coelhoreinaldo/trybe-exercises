2c7c487cd82da15c458f588167dba81dc41d8c62
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-26 15:04:29 -0300

    feat: 8-3 exercise2-5 reduce

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/exercises/exercise2.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions-filter-e-reduce/exercises/exercise2.js
@@ -21,12 +21,6 @@ const longestName = () => {
 
 longestName(); //país com maior numero de caracteres no nome
 
-const names = [
-  'Aanemarie', 'Adervandes', 'Akifusa',
-  'Abegildo', 'Adicellia', 'Aladonata',
-  'Abeladerco', 'Adieidy', 'Alarucha',
-];
-
 const countA = () => {
   const names = data.countries.map((element) => element.name);
   const namesLetters = names.join('').toLowerCase().split('');
@@ -34,4 +28,4 @@ const countA = () => {
   return namesLetters.reduce((acc, curr) => curr === 'a' ? acc += 1 : acc, 0)
 }
 
-countA();
+countA(); //conta as letras A dos nomes dos países
