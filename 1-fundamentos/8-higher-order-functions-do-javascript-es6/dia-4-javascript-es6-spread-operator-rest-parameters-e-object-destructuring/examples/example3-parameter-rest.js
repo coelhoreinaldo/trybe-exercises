6296da15944cdafa64d76beeff60297abdf1c3ad
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-27 10:16:23 -0300

    feat: 8-4 example 4 parameter rest

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/examples/example3-parameter-rest.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/examples/example3-parameter-rest.js
@@ -3,6 +3,8 @@ const countParams = (...args) => {
   return `Você passou ${args.length} parâmetros para a função.`;
 }
 
-console.log(countParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
-console.log(countParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
+// console.log(countParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+// console.log(countParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
 
+const sum = (...args) => args.reduce((accumulator, current) => accumulator + current, 0);
+console.log(sum(4, 7, 8, 9, 60)); // 88
