0330812d2ac8117d1101b25c4c9a4f76991a8ce1
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-27 11:40:36 -0300

    feat: 8-4 example 4 object destructuring

--- 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/examples/example4-object-destructuring.js
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/examples/example4-object-destructuring.js
@@ -4,6 +4,26 @@ const product = {
   seller: 'Casas de Minas',
 };
 
-const { name, seller } = product;
-console.log(name); // Smart TV Crystal UHD
-console.log(seller); // Casas de Minas
+// const { name, seller } = product;
+// console.log(name); // Smart TV Crystal UHD
+// console.log(seller); // Casas de Minas
+
+// definindo o objeto
+const character = {
+  name: 'Luke SkyWalker',
+  age: '53',
+  description: {
+    specieName: 'Human',
+    jedi: true,
+  },
+  homeWorld: {
+    name: 'Tatooine',
+    population: '200000',
+  },
+};
+
+// desestruturando o objeto:
+const { name, age, homeWorld: { name: planetName }, description: { jedi } } = character;
+
+// imprimindo os valores:
+console.log(`Esse é o ${name}, ele tem ${age} anos, mora no planeta ${planetName} e, por incrível que possa parecer, ele ${jedi ? 'é um Jedi' : 'não é um Jedi'}.`);
