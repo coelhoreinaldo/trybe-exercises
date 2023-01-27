adc07be6e66702095106f0d26fbbf0e04e310364
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-27 11:46:16 -0300

    feat: 8-4 example 6 object destructuring

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/examples/example6-object-destructuring.js
@@ -0,0 +1,23 @@
+const student = {
+  a: 'Maria',
+  b: 'Turma B',
+  c: 'Matematica',
+};
+
+const { a: name, b: classAssigned, c: subject } = student;
+
+console.log(name); // Maria
+console.log(classAssigned); // Turma B
+console.log(subject); // Matemática
+
+const product = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
+
+const printProductDetails = ({ name, price, seller }) => {
+  console.log(`Promoção! ${name} por apenas ${price} é só aqui: ${seller}`);
+};
+
+printProductDetails(product); // Promoção! Smart TV Crystal UHD por apenas 1899.05 é só aqui: Casas de Minas
