1d327032a797e8fe1994dc39206648fe629d7167
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-27 11:37:33 -0300

    feat: 8-4 example 4 object destructuring

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/examples/example4-object-destructuring.js
@@ -0,0 +1,9 @@
+const product = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
+
+const { name, seller } = product;
+console.log(name); // Smart TV Crystal UHD
+console.log(seller); // Casas de Minas
