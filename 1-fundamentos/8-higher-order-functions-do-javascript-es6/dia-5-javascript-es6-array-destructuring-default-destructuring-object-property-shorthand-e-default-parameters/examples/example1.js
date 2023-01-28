7d55618be9d9df00cc901edaafbefb53cbca71a2
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-28 17:41:12 -0300

    feat: example

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/examples/example1.js
@@ -0,0 +1,14 @@
+const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+
+// const firstCountry = arrayCountries[0];
+// const secondCountry = arrayCountries[1];
+// const thirdCountry = arrayCountries[2];
+// const fourthCountry = arrayCountries[3];
+
+const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountries;
+
+console.log(firstCountry); // Brazil
+console.log(secondCountry); // Japan
+console.log(thirdCountry); // China
+console.log(fourthCountry); // Canada
+
