c0ce6273fff3a56a4c404994ab4864dd9be98f8c
coelhoreinaldo <scoelhor7@gmail.com>
2023-01-28 20:55:02 -0300

    feat: exerise 2

---
+++ 1-fundamentos/8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/exercises/exercise2.js
@@ -0,0 +1,7 @@
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const toObject = ([type, brand, year]) => ({type, brand, year})
+
+console.log(toObject(palio))
