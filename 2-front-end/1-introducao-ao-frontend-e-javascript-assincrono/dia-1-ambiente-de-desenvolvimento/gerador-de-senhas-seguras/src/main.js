872ba44f305a3d9e6e1fb3a55622134b23697006
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-03 18:18:07 -0300

    feat: clipboard copy at button

--- 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/gerador-de-senhas-seguras/src/main.js
+++ 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/gerador-de-senhas-seguras/src/main.js
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 import './style.css';
+import copy from 'clipboard-copy';
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
@@ -7,4 +8,8 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
     const randomPassword = nanoid();
     displayPasswordEl.innerHTML = randomPassword;
+});
+
+passwordBtnEl.addEventListener('click', () => {
+    copy(displayPasswordEl.innerHTML);
 });
