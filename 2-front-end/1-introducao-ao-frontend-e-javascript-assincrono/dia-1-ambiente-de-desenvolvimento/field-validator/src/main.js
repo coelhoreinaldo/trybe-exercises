921f5633c3cef20cd61e2395b18792f90e6702cf
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-04 08:41:44 -0300

    feat: main js and validator from validator framework

---
+++ 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/field-validator/src/main.js
@@ -0,0 +1,22 @@
+import validator from 'validator';
+
+const inputText = document.getElementById('input-text');
+const validateBtn = document.getElementById('validate-btn');
+const typeSelected = document.getElementById('type-selected');
+const answer = document.getElementById('answer');
+
+// console.log(isEmail(inputText [, options]));
+
+validateBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const field = {
+    cpf: validator.isTaxID(inputText.value, 'pt-BR'),
+    email: validator.isEmail(inputText.value),
+    password: validator.isStrongPassword(inputText.value),
+    number: validator.isDivisibleBy(inputText.value, 2),
+    lowercase: validator.isLowercase(inputText.value),
+  }
+
+  answer.innerHTML = `A validação retornou ${field[typeSelected.value]}`
+})
