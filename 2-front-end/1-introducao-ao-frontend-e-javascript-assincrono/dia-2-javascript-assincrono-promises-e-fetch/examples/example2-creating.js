7af3f822b1902683b8cf844501d6f1375b5e746c
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-05 10:03:56 -0300

    feat: then n catch

--- 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-2-javascript-assincrono-promises-e-fetch/examples/example2-creating.js
+++ 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-2-javascript-assincrono-promises-e-fetch/examples/example2-creating.js
@@ -9,11 +9,28 @@ const resolvedPromise = () =>
       resolve(randomNumber);
     }, 1000);
   });
-  
-  const rejectedPromise = () =>
+
+// promise rejeitada retornando um objeto de erro
+const rejectedPromise = () =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
       const randomNumber = generateRandomNumber();
       reject(new Error(`O número ${randomNumber} é inválido.`));
     }, 1000);
+  });
+
+resolvedPromise()
+  .then((response) => {
+    console.log(`resolvedPromise: o número gerado é ${response}.`);
+  })
+  .catch((error) => {
+    console.log(`Com a promise resolvida, não irá passar pelo catch`);
+  });
+
+rejectedPromise()
+  .then((response) => {
+    console.log(`Com a promise rejeitada, não irá passar pelo then`);
+  })
+  .catch((error) => {
+    console.log(`rejectedPromise: ${error.message}`);
   });
