1d01e8ffcec870e82183ac05986a8cdfb0c717f5
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-06 17:48:07 -0300

    feat: dog btn

---
+++ 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-2-javascript-assincrono-promises-e-fetch/bonus/src/main.js
@@ -0,0 +1,11 @@
+const dogBtn = document.getElementById('random-dog');
+const catBtn = document.getElementById('random-cat');
+const surpriseBtn = document.getElementById('surprise');
+const image = document.getElementById('random-pet');
+
+dogBtn.addEventListener(('click'), () => {
+
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then((response) => response.json())
+    .then((data) => image.src = data.message)
+});
