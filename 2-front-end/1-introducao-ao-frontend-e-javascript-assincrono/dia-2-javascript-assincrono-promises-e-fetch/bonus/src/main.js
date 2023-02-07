0ecbb721e2776cf92b0fff48dc72d7ae937b4fe8
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-07 10:11:56 -0300

    build: eslint

--- 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-2-javascript-assincrono-promises-e-fetch/bonus/src/main.js
+++ 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-2-javascript-assincrono-promises-e-fetch/bonus/src/main.js
@@ -1,34 +1,32 @@
-import "./style.css"
+/* eslint-disable no-return-assign */
+import './style.css';
 
 const dogBtn = document.getElementById('random-dog');
 const catBtn = document.getElementById('random-cat');
 const surpriseBtn = document.getElementById('surprise');
 const image = document.getElementById('random-pet');
 
-
 dogBtn.addEventListener(('click'), () => {
-
-  fetch('https://dog.ceo/api/breeds/image/random')
-    .then((response) => response.json())
-    .then((data) => image.src = data.message)
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then((response) => response.json())
+        .then((data) => image.src = data.message);
 });
 
 catBtn.addEventListener(('click'), () => {
-
-  fetch('https://aws.random.cat/meow')
-    .then((response) => response.json())
-    .then((data) => image.src = data.file)
+    fetch('https://aws.random.cat/meow')
+        .then((response) => response.json())
+        .then((data) => image.src = data.file);
 });
 
 surpriseBtn.addEventListener(('click'), () => {
-  Promise.any([
-    fetch('https://dog.ceo/api/breeds/image/random'),
-    fetch('https://aws.random.cat/meow'),
-  ])
-  .then((response) => response.json())
-  .then((data) => {
-    const finalUrl = data.message || data.file
+    Promise.any([
+        fetch('https://dog.ceo/api/breeds/image/random'),
+        fetch('https://aws.random.cat/meow'),
+    ])
+        .then((response) => response.json())
+        .then((data) => {
+            const finalUrl = data.message || data.file;
 
-    image.src = finalUrl
-  })
+            image.src = finalUrl;
+        });
 });
