947dddc44078c5ad14c2ebe653a83e8f5db0992a
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-08 19:24:45 -0300

    feat: moedas

--- 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-3-pratica-casa-de-cambio/src/main.js
+++ 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-3-pratica-casa-de-cambio/src/main.js
@@ -1,14 +1,25 @@
 const searchButton = document.getElementById('search');
-const currencyContainer = document.getElementById('currency-container');
-const resultH1 = document.getElementById('resultH1');
+const currencyUl = document.getElementById('currency-ul');
+const resultH1 = document.getElementById('result');
 
 searchButton.addEventListener('click', (event) => {
   event.preventDefault();
 
   const moeda = document.getElementById('moeda').value;
+
   fetch(`https://api.exchangerate.host/latest?base=${moeda}`)
     .then((response) => response.json())
     .then((data) => {
-
+      resultH1.innerHTML = `Valores referentes a 1 ${moeda}`;
+      const { rates } = data;
+      for (const [key, value] of Object.entries(rates)) {
+        const moedas = document.createElement('li');
+        currencyUl.appendChild(moedas);
+        moedas.innerHTML = `${key} ${value}`;
+      }
+      // Object.entries(rates).forEach((m, index) => {
+      //   moedas.innerHTML = `${m}`;
+      //   console.log(m);
+      // });
     });
 });
