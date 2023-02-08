9307f83f0d4909d133ea65ab9f6f4b914a78610e
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-08 18:49:11 -0300

    build: eslint da trybe

--- 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-3-pratica-casa-de-cambio/src/main.js
+++ 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-3-pratica-casa-de-cambio/src/main.js
@@ -1,5 +1,16 @@
+const searchButton = document.getElementById('search');
+const currencyContainer = document.getElementById('currency-container');
+const resultH1 = document.getElementById('resultH1');
 
+searchButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  
+  const moeda = document.getElementById('moeda').value;
+  fetch(`https://api.exchangerate.host/latest?base=${moeda}`)
+      .then((response) => response.json())
+      .then((data) => {
+        
+      });
+  
+});
 
-fetch('https://api.exchangerate.host/latest?base=${moeda}')
-    .then((response) => response.json())
-    .then((data) => console.log(data.rates));
