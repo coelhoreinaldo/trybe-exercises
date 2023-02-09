74009371c98f0b507f2cdc7e79b4f842a83df90b
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-09 09:40:16 -0300

    feat: !!!!!!!!!!

--- 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-3-pratica-casa-de-cambio/src/main.js
+++ 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-3-pratica-casa-de-cambio/src/main.js
@@ -12,13 +12,22 @@ searchButton.addEventListener('click', (event) => {
   fetch(`https://api.exchangerate.host/latest?base=${moeda}`)
     .then((response) => response.json())
     .then((data) => {
-      resultH1.innerHTML = `Valores referentes a 1 ${moeda}`;
       const { rates } = data;
+      if (!Object.keys(rates).includes(moeda)) {
+        throw new Error('Escreva o nome válido.');
+      }
+      resultH1.innerHTML = `Valores referentes a 1 ${moeda}`;
       currencyUl.innerHTML = '';
       Object.entries(rates).forEach(([key, value]) => {
         const moedas = document.createElement('li');
         currencyUl.appendChild(moedas);
         moedas.innerHTML = `${key} ${value}`;
       });
-    });
+    })
+    .catch((error) => Swal.fire({
+      title: 'Moeda não encontrada',
+      text: error.message,
+      icon: 'error',
+      confirmButtonText: 'Cool',
+    }));
 });
