2ea4625c68d15244b235c98f23d37be3235b62c2
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-12 16:16:48 -0300

    feat: izi

--- 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-2-javascript-assincrono-promises-e-fetch/super-heroes/src/main.js
+++ 2-front-end/1-introducao-ao-frontend-e-javascript-assincrono/dia-2-javascript-assincrono-promises-e-fetch/super-heroes/src/main.js
@@ -1,10 +1,19 @@
+import './style.css';
+
+const button = document.getElementById('button');
+const cardImg = document.getElementById('image');
+const herosName = document.getElementById('name');
+
+const MAX_HEROES = 731;
 
 
-const meme = () => {
-  const randomNumber = Math.round(Math.random() * 731)
-
+button.addEventListener('click', (event) => {
+  const randomNumber = Math.round(Math.random() * MAX_HEROES)
   fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${randomNumber}.json`)
   .then((response) => response.json())
-  .then((data) => console.log(data))
-}
-meme()
+  .then((data) => {
+    cardImg.src = data.images.sm;
+    herosName.innerHTML = data.name
+  })
+})
+
