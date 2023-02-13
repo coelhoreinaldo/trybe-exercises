a78936184d88ca830866210038c74c02ef893111
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-13 10:39:18 -0300

    feat: exercise-1 h2 n ol list

--- 2-front-end/2-introducao-ao-react/dia-1-hello-world-no-react/exercise-1/src/About.js
+++ 2-front-end/2-introducao-ao-react/dia-1-hello-world-no-react/exercise-1/src/About.js
@@ -6,7 +6,13 @@ class About extends React.Component {
       <main>
       <h1>Reinaldo Coelho</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-
+      <h2>Minhas Habilidades</h2>
+      <ol>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Git & GitHub</li>
+      </ol>
       </main>
     )
   }
