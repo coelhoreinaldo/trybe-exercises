6acdb9721cf45cdc330c1cb293d0c0aadc033ea6
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-13 10:39:08 -0300

    feat: exercise-1 h1 n p

---
+++ 2-front-end/2-introducao-ao-react/dia-1-hello-world-no-react/exercise-1/src/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <main>
+      <h1>Reinaldo Coelho</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+
+      </main>
+    )
+  }
+}
