e565269b7d4f24e8179a034df22e36ab094455a8
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-13 10:42:33 -0300

    feat: exercise-1 export n import

--- 2-front-end/2-introducao-ao-react/dia-1-hello-world-no-react/exercise-1/src/About.js
+++ 2-front-end/2-introducao-ao-react/dia-1-hello-world-no-react/exercise-1/src/About.js
@@ -3,17 +3,14 @@ import React, { Component } from "react";
 class About extends React.Component {
   render() {
     return (
-      <main>
-      <h1>Reinaldo Coelho</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-      <h2>Minhas Habilidades</h2>
-      <ol>
+      <main><h1>Reinaldo Coelho</h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p><h2>Minhas Habilidades</h2><ol>
         <li>HTML</li>
         <li>CSS</li>
         <li>JavaScript</li>
         <li>Git & GitHub</li>
-      </ol>
-      </main>
+      </ol></main>
     )
   }
 }
+
+export default About;
--- 2-front-end/2-introducao-ao-react/dia-1-hello-world-no-react/exercise-1/src/App.js
+++ 2-front-end/2-introducao-ao-react/dia-1-hello-world-no-react/exercise-1/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import About from './About';
 
 class App extends React.Component {
   render() {
-    return ();
+    return (<About></About>);
   }
 }
 
