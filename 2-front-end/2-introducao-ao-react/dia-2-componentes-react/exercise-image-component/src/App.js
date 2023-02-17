c2fb7728baf176abcb50c183ca3aac6d2f1b82a9
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-17 15:29:47 -0300

    feat: refactoring app.js

--- 2-front-end/2-introducao-ao-react/dia-2-componentes-react/exercise-image-component/src/App.js
+++ 2-front-end/2-introducao-ao-react/dia-2-componentes-react/exercise-image-component/src/App.js
@@ -1,9 +1,9 @@
 // src/App.js
 import React from 'react';
+import Album from './components/Album'
 
 class App extends React.Component {
   render() {
-    // Declaração do objeto contendo informações do album01
     const album01 = {
       image: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Coldplay_-_Mylo_Xyloto_%28Official_Album_Cover%29.png/220px-Coldplay_-_Mylo_Xyloto_%28Official_Album_Cover%29.png',
       title: 'Mylo Xyloto',
@@ -18,7 +18,6 @@ class App extends React.Component {
       }
     };
 
-    // Declaração do objeto contendo informações do album02
     const album02 = {
       image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
       title: 'Ghost Stories',
@@ -33,24 +32,11 @@ class App extends React.Component {
       }
     };
 
-    // Retorno do que será renderizado
     return (
-      <article>
-        <section>
-          <img src={ album01.image } alt={ album01.title } />
-          <h2>{ album01.title }</h2>
-          <p>Lançamento: { album01.releaseDate.year }</p>
-          <p>Gravadora: { album01.others.recordCompany }</p>
-          <p>Formatos: { album01.others.formats }</p>
-        </section>
-        <section>
-          <img src={ album02.image } alt={ album02.title } />
-          <h2>{ album02.title }</h2>
-          <p>Lançamento: { album02.releaseDate.year }</p>
-          <p>Gravadora: { album02.others.recordCompany }</p>
-          <p>Formatos: { album02.others.formats }</p>
-        </section>
-      </article>
+      <div>
+        <Album album={ album01 } />
+        <Album album={ album02 } />
+      </div>
     );
   }
 }
