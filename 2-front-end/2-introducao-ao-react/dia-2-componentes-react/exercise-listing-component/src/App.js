66f2adb5e2903ab332ff3430db5a9e7edbc80c80
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-18 11:20:07 -0300

    feat: app hof map

--- 2-front-end/2-introducao-ao-react/dia-2-componentes-react/exercise-listing-component/src/App.js
+++ 2-front-end/2-introducao-ao-react/dia-2-componentes-react/exercise-listing-component/src/App.js
@@ -5,24 +5,24 @@ import UserProfile from './components/UserProfile';
 
 class App extends React.Component {
   render() {
-    const joao = {
-      id: 102,
-      name: 'João',
-      email: 'joao@gmail.com',
-      avatar: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png'
-    };
-
-    const amelia = {
-      id: 77,
-      name: 'Amélia',
-      email: 'amelia@gmail.com',
-      avatar: 'https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png'
-    };
+    const users = [
+      {
+        id: 102,
+        name: 'João',
+        email: 'joao@gmail.com',
+        avatar: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png'
+      },
+      {
+        id: 77,
+        name: 'Amélia',
+        email: 'amelia@gmail.com',
+        avatar: 'https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png'
+      }
+    ];
 
     return (
       <div className="App">
-        <UserProfile user={ joao } />
-        <UserProfile user={ amelia } />
+        {users.map((user) => <UserProfile user={ user } />)}
       </div>
     );
   }
