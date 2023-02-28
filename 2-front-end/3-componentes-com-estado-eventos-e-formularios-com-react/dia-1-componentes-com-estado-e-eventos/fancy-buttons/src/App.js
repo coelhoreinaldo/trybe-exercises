17aa851048ff807bb63f187ea42647982b1e03fd
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-28 11:35:56 -0300

    feat: this.setState

--- 2-front-end/3-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 2-front-end/3-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -10,23 +10,39 @@ class App extends React.Component {
   //não precisa do constructor pois foi usada a sintaxe de arrow function
 
   state = {
-    clicksCount: 0,
+    button1ClicksCount: 0,
+    button2ClicksCount: 0,
+    button3ClicksCount: 0,
   }
 
-   handleClick = () => {
+   handleClick1 = () => {
     this.setState((previousState, _props) => ({
-      clicksCount: previousState.clicksCount + 1,
+      button1ClicksCount: previousState.button1ClicksCount + 1,
     }));
-    console.log(Math.round(Math.random() * 10));
     console.log(this);
   };
-  render(){
 
+   handleClick2 = () => {
+    this.setState((previousState, _props) => ({
+      button2ClicksCount: previousState.button2ClicksCount + 1,
+    }));
+    console.log(this);
+  };
+  
+   handleClick3 = () => {
+    this.setState((previousState, _props) => ({
+      button3ClicksCount: previousState.button3ClicksCount + 1,
+    }));
+    console.log(this);
+  };
+
+  render(){
+    const { button1ClicksCount, button2ClicksCount, button3ClicksCount } = this.state;  
     return (
       <div className="App">
-        <button key={1} onClick={this.handleClick}>Botão 1</button>
-        <button key={2} onClick={this.handleClick}>Botão 2</button>
-        <button key={3} onClick={this.handleClick}>Botão 3</button>
+        <button onClick={this.handleClick1}>{`Número de cliques: ${button1ClicksCount}`}</button>
+        <button onClick={this.handleClick2}>{`Número de cliques: ${button2ClicksCount}`}</button>
+        <button onClick={this.handleClick3}>{`Número de cliques: ${button3ClicksCount}`}</button>
       </div>
     );
   }
