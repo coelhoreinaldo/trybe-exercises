946ec9d7bb4759b6825a233372318a8571dec0a1
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-28 11:27:06 -0300

    feat: this.setState

--- 2-front-end/3-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 2-front-end/3-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -4,11 +4,19 @@ import React from 'react';
 class App extends React.Component {
   // constructor() {
   //   super();
-  //   this.handleClick = this.handleClick.bind(this)
+  //   // this.handleClick = this.handleClick.bind(this)
   // }
 
   //não precisa do constructor pois foi usada a sintaxe de arrow function
+
+  state = {
+    clicksCount: 0,
+  }
+
    handleClick = () => {
+    this.setState((previousState, _props) => ({
+      clicksCount: previousState.clicksCount + 1,
+    }));
     console.log(Math.round(Math.random() * 10));
     console.log(this);
   };
@@ -16,9 +24,9 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <button onClick={this.handleClick}>onClick</button>
-        <button onMouseOver={this.handleClick}>onMouseOver</button>
-        <button onMouseLeave={this.handleClick}>onMouseLeave</button>
+        <button key={1} onClick={this.handleClick}>Botão 1</button>
+        <button key={2} onClick={this.handleClick}>Botão 2</button>
+        <button key={3} onClick={this.handleClick}>Botão 3</button>
       </div>
     );
   }
