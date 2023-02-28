3789bfe9634170dec5f739f62d1fb70f22d4570e
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-28 18:37:47 -0300

    buttons

--- 2-front-end/3-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 2-front-end/3-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -15,34 +15,34 @@ class App extends React.Component {
     button3ClicksCount: 0,
   }
 
-   handleClick1 = () => {
+  
+  handleClick1 = () => {
     this.setState((previousState, _props) => ({
       button1ClicksCount: previousState.button1ClicksCount + 1,
     }));
-    console.log(this);
   };
 
    handleClick2 = () => {
     this.setState((previousState, _props) => ({
       button2ClicksCount: previousState.button2ClicksCount + 1,
     }));
-    console.log(this);
   };
   
    handleClick3 = () => {
     this.setState((previousState, _props) => ({
       button3ClicksCount: previousState.button3ClicksCount + 1,
     }));
-    console.log(this);
   };
+
+  buttonColor = (num) => num % 2 === 0 ? 'green' : 'white';
 
   render(){
     const { button1ClicksCount, button2ClicksCount, button3ClicksCount } = this.state;  
     return (
       <div className="App">
-        <button onClick={this.handleClick1}>{`Número de cliques: ${button1ClicksCount}`}</button>
-        <button onClick={this.handleClick2}>{`Número de cliques: ${button2ClicksCount}`}</button>
-        <button onClick={this.handleClick3}>{`Número de cliques: ${button3ClicksCount}`}</button>
+        <button onClick={this.handleClick1} style={{backgroundColor: this.buttonColor(button1ClicksCount)}}>{`Número de cliques: ${button1ClicksCount}`}</button>
+        <button onClick={this.handleClick2} style={{backgroundColor: this.buttonColor(button2ClicksCount)}}>{`Número de cliques: ${button2ClicksCount}`}</button>
+        <button onClick={this.handleClick3} style={{backgroundColor: this.buttonColor(button3ClicksCount)}}>{`Número de cliques: ${button3ClicksCount}`}</button>
       </div>
     );
   }
