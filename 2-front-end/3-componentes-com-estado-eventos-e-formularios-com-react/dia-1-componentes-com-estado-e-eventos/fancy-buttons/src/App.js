730ba4463a45e6ea6bb64a14fd40ee1fbf4b4a5e
coelhoreinaldo <scoelhor7@gmail.com>
2023-02-28 18:42:34 -0300

    buttons console.log

--- 2-front-end/3-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 2-front-end/3-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
-  // constructor() {
-  //   super();
-  //   // this.handleClick = this.handleClick.bind(this)
-  // }
+  constructor() {
+    super();
+    this.handleClick1 = this.handleClick1.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+    this.handleClick3 = this.handleClick3.bind(this)
+    this.buttonColor = this.buttonColor.bind(this)
+  }
 
   //não precisa do constructor pois foi usada a sintaxe de arrow function
 
@@ -15,34 +18,44 @@ class App extends React.Component {
     button3ClicksCount: 0,
   }
 
-  
-  handleClick1 = () => {
+  handleClick1() {
+    const { button1ClicksCount } = this.state;
     this.setState((previousState, _props) => ({
       button1ClicksCount: previousState.button1ClicksCount + 1,
-    }));
+    }), () => {
+      console.log(`Botão 1 ${this.buttonColor(button1ClicksCount)}`)
+    });
   };
 
-   handleClick2 = () => {
+  handleClick2() {
+    const { button2ClicksCount } = this.state;
     this.setState((previousState, _props) => ({
       button2ClicksCount: previousState.button2ClicksCount + 1,
-    }));
+    }), () => {
+      console.log(`Botão 2 ${this.buttonColor(button2ClicksCount)}`)
+    });
   };
-  
-   handleClick3 = () => {
+
+  handleClick3() {
+    const { button3ClicksCount } = this.state;
     this.setState((previousState, _props) => ({
       button3ClicksCount: previousState.button3ClicksCount + 1,
-    }));
+    }), () => {
+      console.log(`Botão 3 ${this.buttonColor(button3ClicksCount)}`)
+    });
   };
 
-  buttonColor = (num) => num % 2 === 0 ? 'green' : 'white';
+  buttonColor(num) {
+    return num % 2 === 0 ? 'green' : 'white'
+  };
 
-  render(){
-    const { button1ClicksCount, button2ClicksCount, button3ClicksCount } = this.state;  
+  render() {
+    const { button1ClicksCount, button2ClicksCount, button3ClicksCount } = this.state;
     return (
       <div className="App">
-        <button onClick={this.handleClick1} style={{backgroundColor: this.buttonColor(button1ClicksCount)}}>{`Número de cliques: ${button1ClicksCount}`}</button>
-        <button onClick={this.handleClick2} style={{backgroundColor: this.buttonColor(button2ClicksCount)}}>{`Número de cliques: ${button2ClicksCount}`}</button>
-        <button onClick={this.handleClick3} style={{backgroundColor: this.buttonColor(button3ClicksCount)}}>{`Número de cliques: ${button3ClicksCount}`}</button>
+        <button onClick={this.handleClick1} style={{ backgroundColor: this.buttonColor(button1ClicksCount) }}>{`Número de cliques: ${button1ClicksCount}`}</button>
+        <button onClick={this.handleClick2} style={{ backgroundColor: this.buttonColor(button2ClicksCount) }}>{`Número de cliques: ${button2ClicksCount}`}</button>
+        <button onClick={this.handleClick3} style={{ backgroundColor: this.buttonColor(button3ClicksCount) }}>{`Número de cliques: ${button3ClicksCount}`}</button>
       </div>
     );
   }
