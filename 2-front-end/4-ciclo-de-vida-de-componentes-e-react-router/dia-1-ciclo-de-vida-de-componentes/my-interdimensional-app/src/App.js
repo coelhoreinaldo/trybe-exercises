666493bad55196e46f0d72a8d087d7fc71b50ab7
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-09 10:02:26 -0300

    feat: render

--- 2-front-end/4-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/my-interdimensional-app/src/App.js
+++ 2-front-end/4-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/my-interdimensional-app/src/App.js
@@ -19,11 +19,22 @@ class App extends Component {
   }
 
   render() {
+    const { characters } = this.state;
     return (
       <div className="App">
         <h1>
           Ricky and Morty Characters:
         </h1>
+        <div className="body">
+          {characters.map(({ name, image }) => {
+            return (
+              <div className="container" key={name}>
+                <h3>{name}</h3>
+                <img src={image} alt={name}/>
+              </div>
+            )
+          })}
+        </div>
       </div>
     );
   }
