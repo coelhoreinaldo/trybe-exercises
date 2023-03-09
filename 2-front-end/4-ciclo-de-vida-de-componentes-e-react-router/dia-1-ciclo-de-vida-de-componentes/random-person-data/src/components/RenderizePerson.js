1533b378785f9dd9e335896e89eed0534af5bf7e
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-09 12:16:25 -0300

    feat: shouldComponent

--- 2-front-end/4-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/random-person-data/src/components/RenderizePerson.js
+++ 2-front-end/4-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/random-person-data/src/components/RenderizePerson.js
@@ -12,23 +12,42 @@ export default class RenderizePerson extends React.Component {
 
   }
 
-  async componentDidMount() {
-    this.setState({ loading: true }, async () => {
-
-      const fetchApi = await fetch('https://api.randomuser.me/');
-      const response = await fetchApi.json();
-      this.setState({ randomPerson: response.results, loading: false })
-    }
-    )
+  fetchApi = async () => {
+    const API_URL = 'https://api.randomuser.me/';
+    const fetchApi = await fetch(API_URL);
+    const response = await fetchApi.json();
+    this.setState({ randomPerson: response.results, loading: false })
   }
 
-  // shouldComponentUpdate(){
-  //   const { randomPerson } = this.state
-  //   if(randomPerson[0].dob.age > 50){
-  //     return false
-  //   }
-  //   return true;
-  // }
+  async componentDidMount() {
+    // Isso deu erro porque no primeiro log, o should update dá undefined
+    // const API_URL = 'https://api.randomuser.me/';
+    // this.setState(async () => {
+
+    //   const fetchApi = await fetch(API_URL);
+    //   const response = await fetchApi.json();
+    //   this.setState({ randomPerson: response.results, loading: false })
+    // })
+    this.fetchApi();
+    // Segunda forma de fazer, com o then    
+    // fetch(API_URL)
+    // .then((response) => response.json())
+    // .then((data) => {
+    //   this.setState({
+    //     loading: false,
+    //     randomPerson: data.results,
+    //   })
+    // })
+  }
+
+  shouldComponentUpdate(_nextProps, nextState) {
+    console.log(nextState.randomPerson[0]);
+    const AGE = 50;
+    if (nextState.randomPerson[0].dob.age > AGE) {
+      return false;
+    }
+    return true
+  }
 
   render() {
     const { loading, randomPerson } = this.state;
@@ -36,11 +55,11 @@ export default class RenderizePerson extends React.Component {
       <div>
         {
           loading ? <span>Loading...</span> : randomPerson.map(({
-             name, login, picture, email, dob 
-            }) => {
+            name, login, picture, email, dob
+          }) => {
             return (
               <div key={login.uuid}>
-                <img src={picture.large} alt="foto"/>
+                <img src={picture.large} alt="foto" />
                 <p>Nome: {name.title} {name.first} {name.last}</p>
                 <p>Email: {email}</p>
                 <p>Idade: {dob.age}</p>
