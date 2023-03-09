f02e408d66b54b3a34a2e0f800c59c02d1484b95
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-09 10:00:50 -0300

    feat: fetch api

--- 2-front-end/4-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/my-interdimensional-app/src/App.js
+++ 2-front-end/4-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/my-interdimensional-app/src/App.js
@@ -1,11 +1,32 @@
+// App.js
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        characters: [],
+    };
+  }
+
+  fetchCharacters = () => {
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({characters: data.results})
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Ricky and Morty Characters:
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
