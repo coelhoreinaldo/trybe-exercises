efe1368c0da648afbcd660ec94bfd5c0126e87f7
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-10 11:18:17 -0300

    feat: home history push

--- 2-front-end/4-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/exemplo-history/src/pages/Home.jsx
+++ 2-front-end/4-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/exemplo-history/src/pages/Home.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 class Home extends React.Component {
   render() {
+    const { history } = this.props;
     return (
-      <h1>Home Page</h1>
+      <>
+        <h1>Home Page</h1>
+        <button
+          type="button"
+          onClick={ () => history.push('/welcome') }
+        >
+          Acesse a página de Boas-Vindas
+        </button>
+      </>
     );
   }
 }
