e70ffd982ff042c4f07b26bdf81cfbb4083c8d37
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-10 11:19:17 -0300

    feat: location state

--- 2-front-end/4-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/exemplo-history/src/pages/Home.jsx
+++ 2-front-end/4-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/exemplo-history/src/pages/Home.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: 'Tryber',
+      role: 'Admin',
+    };
+  }
+
   render() {
     const { history } = this.props;
     return (
@@ -8,7 +16,7 @@ class Home extends React.Component {
         <h1>Home Page</h1>
         <button
           type="button"
-          onClick={ () => history.push('/welcome') }
+          onClick={ () => history.push('/welcome', this.state) }
         >
           Acesse a página de Boas-Vindas
         </button>
