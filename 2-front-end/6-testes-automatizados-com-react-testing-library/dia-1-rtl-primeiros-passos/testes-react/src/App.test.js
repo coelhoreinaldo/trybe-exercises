6f1ff83f57f08e755de2ecba250da76ba0bedba0
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-23 11:29:52 -0300

    test: comment

--- 2-front-end/6-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
+++ 2-front-end/6-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+// import userEvent from '@testing-library/user-event';
+
 
 test('Verificando se existe o campo Email.', () => {
   render(<App />);
@@ -22,3 +24,21 @@ test('Verificando se existe um botão de enviar', () => {
   expect(btnSend).toHaveProperty('type', 'button');
   expect(btnSend).toHaveValue('Enviar');
 });
+
+// test('Verificando se o botão e o campo email estão funcionando.', () => {
+//   render(<App />);
+
+//   const EMAIL_USER = 'email@email.com';
+
+//   const textEmail = screen.getByTestId('id-email-user');
+//   expect(textEmail).toBeInTheDocument();
+//   expect(textEmail).toHaveTextContent('Valor:');
+
+//   const btnSend = screen.getByTestId('id-send');
+//   const inputEmail = screen.getByLabelText('Email');
+//   userEvent.type(inputEmail, EMAIL_USER);
+//   userEvent.click(btnSend);
+
+//   expect(inputEmail).toHaveValue('');
+//   expect(textEmail).toHaveTextContent(`Valor: ${ EMAIL_USER }`);
+// });
