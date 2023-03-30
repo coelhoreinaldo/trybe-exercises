81d3548ea0aa6872d378baa9f3aa18c51967cdea
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-30 11:24:07 -0300

    feat: counter conclusion

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/redux-vanilla/index.js
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/redux-vanilla/index.js
@@ -1,6 +1,7 @@
 import { legacy_createStore as createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 
+// 1. Criando o reducer com o estado inicial
 const INITIAL_STATE = { count: 0 }
 
 const reducer = (state = INITIAL_STATE, action) => {
@@ -10,15 +11,21 @@ const reducer = (state = INITIAL_STATE, action) => {
   return state;
 }
 
+
+// 2. Criando a store com o Redux dev tools
 const store = createStore(reducer, composeWithDevTools());
 
+
+// 3. Criando a action
 const action = { type: "INCREMENT_COUNTER" };
 
+// 4. Disparando a action
 const incrementButton = document.querySelector('button');
 incrementButton.addEventListener('click', () => {
   store.dispatch(action);
 })
 
+// 5. Lendo as alterações do estado
 store.subscribe(() => {
   
   const globalState = store.getState();
