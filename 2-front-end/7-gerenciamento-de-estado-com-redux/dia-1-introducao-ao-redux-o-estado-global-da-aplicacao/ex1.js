dc90c8226b76207fbb6f77fae25af2395ca42b13
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-30 10:59:20 -0300

    feat: about redux devtools extension

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/ex1.js
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/ex1.js
@@ -1,7 +1,10 @@
 import { legacy_createStore as createStore } from 'redux';
 
 // Criando a nossa store:
-const store = createStore(reducer);
+const store = createStore(
+  reducer, /* preloadedState, */
+  +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 // Criando uma função Reducer
 const reducer = (state, action) => state
@@ -11,7 +14,7 @@ const action = {
   type: 'INCREMENT_COUNTER'
 };
 
-dispatch({type: 'INCREMENT_COUNTER'});
+dispatch({ type: 'INCREMENT_COUNTER' });
 
 const state = store.getState()
 
