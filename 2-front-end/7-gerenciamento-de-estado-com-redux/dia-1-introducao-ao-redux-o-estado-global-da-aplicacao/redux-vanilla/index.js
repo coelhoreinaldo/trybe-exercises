e569bcaabade776f53d92db0a2c928f5b6e5f59e
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-30 11:16:07 -0300

    feat: action, reducer n dispatch

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/redux-vanilla/index.js
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/redux-vanilla/index.js
@@ -2,13 +2,19 @@ import { legacy_createStore as createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 
 const INITIAL_STATE = { count: 0 }
+
 const reducer = (state = INITIAL_STATE, action) => {
+  if(action.type === "INCREMENT_COUNTER") {
+    return { count: state.count + 1 }
+  }
   return state;
 }
 
 const store = createStore(reducer, composeWithDevTools());
 
+const action = { type: "INCREMENT_COUNTER" };
+
 const incrementButton = document.querySelector('button');
 incrementButton.addEventListener('click', () => {
-  console.log('click');
+  store.dispatch(action);
 })
