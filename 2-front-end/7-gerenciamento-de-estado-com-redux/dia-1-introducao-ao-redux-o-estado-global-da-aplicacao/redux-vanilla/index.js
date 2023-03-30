be40f11f4e0303d6a15807f0a7003fc426b01419
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-30 11:10:22 -0300

    feat: installing redux dev tools

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/redux-vanilla/index.js
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/redux-vanilla/index.js
@@ -1,4 +1,12 @@
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
+const INITIAL_STATE = { count: 0 }
+const reducer = (state = INITIAL_STATE, action) => {
+  return state;
+}
+
+const store = createStore(reducer, composeWithDevTools());
 
 const incrementButton = document.querySelector('button');
 incrementButton.addEventListener('click', () => {
