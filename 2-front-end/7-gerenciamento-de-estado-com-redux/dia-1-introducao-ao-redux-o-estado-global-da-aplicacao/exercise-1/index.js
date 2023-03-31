81ef853cfa42033c9d832b5dd2fd27ced2a6c9bb
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-30 22:42:06 -0300

    feat: add lessons

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercise-1/index.js
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercise-1/index.js
@@ -6,7 +6,11 @@ const INITIAL_STATE = {
   index: 0,
 };
 
+// 2 - Crie um reducer, implementando um switch que retorne apenas o estado inicial como default.
+
 const reducer = (state = INITIAL_STATE, action) => {
+
+  // 3 - Incremente o switch criado no passo anterior com os cases NEXT_COLOR e PREVIOUS_COLOR. Essas actions devem alterar o valor index guardado no INITIAL_STATE.
   switch (action.type) {
     case 'NEXT_COLOR':
       return {
@@ -25,7 +29,12 @@ const reducer = (state = INITIAL_STATE, action) => {
   }
 }
 
+// 1 - Crie uma store para a nossa aplicação.
+
 const store = createStore(reducer, composeWithDevTools())
+
+
+// 4 - Crie eventListeners que escutam os cliques de cada botão, o Previous color e o Next color, e realizam um dispatch com as respectivas actions.
 
 const previousButton = document.getElementById("previous")
 const nextButton = document.getElementById("next")
@@ -48,4 +57,8 @@ store.subscribe(() => {
   currColor.innerHTML = globalState.colors[globalState.index];
   backgroundColor.style.backgroundColor = globalState.colors[globalState.index];
 })
+
+// 6 - Crie um botão com o texto Random color, um eventListener e uma action no reducer, adicionando uma cor aleatória ao array colors guardado em nosso estado inicial. Faça também com que essa cor seja mostrada ao ser criada, alterando o estado index para a posição dela.
+
+
 
