b60c988905bc6a058e2b22b23b08e0d5e6fb041d
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-30 23:03:18 -0300

    feat: req 3

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercise-2/main.js
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercise-2/main.js
@@ -9,7 +9,28 @@ const INITIAL_STATE = {
 // 1 - Crie um reducer com os estados iniciais.
 
 const reducer = (state = INITIAL_STATE, action) => {
+  // 3 - Adicione um case dentro do switch do reducer para alterar os estados theme e status. O case do estado theme deve alterná-lo entre ‘light’ e ‘dark’. O case do estado status deve alterná-lo entre ‘online’ e ‘offline’.
   switch (action.type) {
+    case 'online':
+      return {
+        ...state,
+        status: 'online'
+      }
+    case 'offline':
+      return {
+        ...state,
+        status: 'offline'
+      }
+    case 'light':
+      return {
+        ...state,
+        status: 'light'
+      }
+    case 'dark':
+      return {
+        ...state,
+        status: 'dark'
+      }
     default:
       return state
   }
@@ -18,6 +39,7 @@ const reducer = (state = INITIAL_STATE, action) => {
 // 2 - Crie uma store com o reducer criado.
 
 const store = createStore(reducer, composeWithDevTools());
+
 
 const themeButton = document.getElementById('toggle-theme');
 const statusButton = document.getElementById('toggle-status');
