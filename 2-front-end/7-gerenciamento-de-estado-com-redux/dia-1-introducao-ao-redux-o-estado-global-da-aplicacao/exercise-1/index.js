6647c49a9bbd4cf2dd602891d7e305bfb9fb2409
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-30 22:41:15 -0300

    feat: req 5, subscribe

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercise-1/index.js
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercise-1/index.js
@@ -8,30 +8,44 @@ const INITIAL_STATE = {
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'NEXT_COLOR' && state.index < colors.length:
-      return { index: state.index + 1 }
-      break;
+    case 'NEXT_COLOR':
+      return {
+        ...state,
+        index: state.index === state.colors.length - 1 ? 0 : state.index + 1,
+      }
 
-    case 'PREVIOUS_COLOR' && state.index > 0:
-      return { index: state.index - 1 }
-      break;
+    case 'PREVIOUS_COLOR':
+      return {
+        ...state,
+        index: state.index === 0 ? state.colors.length - 1 : state.index - 1
+      }
 
     default:
       return state
   }
 }
 
-const store = createStore(reducer, composeWithDevTools)
+const store = createStore(reducer, composeWithDevTools())
 
 const previousButton = document.getElementById("previous")
 const nextButton = document.getElementById("next")
 
 previousButton.addEventListener('click', () => {
-  const action = { type: "PREVIOUS_COLOR"}
-  store.dispatch(action)
+  store.dispatch({ type: "PREVIOUS_COLOR" })
 })
 
 nextButton.addEventListener('click', () => {
-  const action = { type: "NEXT_COLOR"}
-  store.dispatch(action)
+  store.dispatch({ type: "NEXT_COLOR" })
 })
+
+// 5 - Faça o subscribe da store, alterando o innerHTML da tag com id value para a cor atual e o style do elemento com id container. Você deverá ser capaz de ver as cores mudando ao fundo e o nome da cor exibida.
+
+store.subscribe(() => {
+  const globalState = store.getState();
+
+  const currColor = document.getElementById('value');
+  const backgroundColor = document.getElementById('container');
+  currColor.innerHTML = globalState.colors[globalState.index];
+  backgroundColor.style.backgroundColor = globalState.colors[globalState.index];
+})
+
