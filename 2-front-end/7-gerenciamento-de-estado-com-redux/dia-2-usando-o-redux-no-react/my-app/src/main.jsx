f3218784bea744c95756049bb820e0f1933fc133
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-31 09:41:24 -0300

    feat: provider from react-redux

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-2-usando-o-redux-no-react/my-app/src/main.jsx
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-2-usando-o-redux-no-react/my-app/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+// ./src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>,
+);
