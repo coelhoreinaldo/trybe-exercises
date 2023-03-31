7ec8f21b3fe29a811365f446619d321c75988a97
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-30 23:00:10 -0300

    feat: reducer initial state

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercise-2/main.js
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercise-2/main.js
@@ -1,7 +1,20 @@
+import { legacy_createStore as createStore } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
 const INITIAL_STATE = {
   status: 'offline',
   theme: 'dark',
 };
+
+// 1 - Crie um reducer com os estados iniciais.
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
 
 const themeButton = document.getElementById('toggle-theme');
 const statusButton = document.getElementById('toggle-status');
