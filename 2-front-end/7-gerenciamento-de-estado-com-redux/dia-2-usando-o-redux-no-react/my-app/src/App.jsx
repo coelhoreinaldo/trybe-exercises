d1831fa892769f523ec55d2ae038895b04fa47a0
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-31 10:12:45 -0300

    feat: dispatch as props, dispatch(actionCreator())

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-2-usando-o-redux-no-react/my-app/src/App.jsx
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-2-usando-o-redux-no-react/my-app/src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css'
 import React from 'react';
 import { connect } from 'react-redux';
+import { actionCreator } from './redux/actions';
 
 class App extends React.Component {
   render() {
-    const { countState } = this.props;
+    const { dispatch, countState } = this.props;
     return (
       <div>
         <h1>O contador</h1>
         <h2>{countState}</h2>
-        <button>Incrementa 1</button>
-        <button>Incrementa 5</button>
+        <button onClick={() => dispatch(actionCreator())}>Incrementa 1</button>
+        <button onClick={() => dispatch(actionCreator(5))}>Incrementa 5</button>
       </div>
     );
   }
