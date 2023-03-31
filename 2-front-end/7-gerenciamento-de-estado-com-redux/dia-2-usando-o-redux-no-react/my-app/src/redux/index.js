79f107f70c1353a816683292fb5744f7d77a63be
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-31 10:10:53 -0300

    feat: action creator

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-2-usando-o-redux-no-react/my-app/src/redux/index.js
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-2-usando-o-redux-no-react/my-app/src/redux/index.js
@@ -1,10 +1,7 @@
 import { legacy_createStore as createStore } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
+import counterReducer from './reducers/counterReducer';
 
-const INITIAL_STATE = { count: 0 };
-
-const reducer = (state = INITIAL_STATE, action) => state;
-
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(counterReducer, composeWithDevTools());
 
 export default store;
