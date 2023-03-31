f3f9188813852e2042b9df8f27cfc3f1062bb355
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-31 09:38:34 -0300

    feat: redux

---
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-2-usando-o-redux-no-react/my-app/src/redux/index.js
@@ -0,0 +1,10 @@
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+const INITIAL_STATE = { count: 0 };
+
+const reducer = (state = INITIAL_STATE, action) => state;
+
+const store = createStore(reducer, composeWithDevTools());
+
+export default store;
