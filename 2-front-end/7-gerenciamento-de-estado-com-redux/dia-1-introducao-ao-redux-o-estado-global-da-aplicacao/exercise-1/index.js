6738f533ccb087622eb660a913fb8b2ecade2d8e
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-30 22:09:32 -0300

    feat: creating reducer

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercise-1/index.js
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/exercise-1/index.js
@@ -1,4 +1,10 @@
+import { legacy_createStore as createStore } from "redux";
+
 const INITIAL_STATE = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
 };
+
+const reducer = (state = INITIAL_STATE, action) => {
+  return state
+}
