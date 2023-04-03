5564887615cbc9c3de929f6b599b2845a814eb03
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-03 10:11:17 -0300

    feat: refactor reduceer

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/my-app-redux/src/redux/reducers/index.js
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-3-usando-o-redux-no-react-pratica/my-app-redux/src/redux/reducers/index.js
@@ -4,6 +4,11 @@ const INITIAL_STATE = { theme: "light" }
 
 const exampleReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case 'ADD_EMAIL':
+      return {
+        ...state,
+        email: action.email,
+      }
     default:
       return state;
   }
