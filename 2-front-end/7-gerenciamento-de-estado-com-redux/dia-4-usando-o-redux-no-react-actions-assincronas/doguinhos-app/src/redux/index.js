fdd86971e16b5b9523203b31d645cff14ca60795
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-04 10:27:26 -0300

    feat: thunk n applymiddleware

--- 2-front-end/7-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/doguinhos-app/src/redux/index.js
+++ 2-front-end/7-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/doguinhos-app/src/redux/index.js
@@ -1,7 +1,8 @@
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import dogReducer from './reducers/dogReducer'
 
-const store = createStore(dogReducer, composeWithDevTools());
+const store = createStore(dogReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
