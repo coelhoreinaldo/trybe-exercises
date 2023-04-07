718729b60048c3bbd1435c43227d190e649a2312
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-07 10:45:04 -0300

    feat🆕: completed

--- 2-front-end/9-context-api-e-react-hooks/dia-1-react-hooks-usestate-useeffect/tryber-forms/src/App.jsx
+++ 2-front-end/9-context-api-e-react-hooks/dia-1-react-hooks-usestate-useeffect/tryber-forms/src/App.jsx
@@ -12,23 +12,60 @@ function App() {
       <h1>Hello World!</h1>
       <form>
         <input
-          name="name"
-          value={ name }
           type="text"
           placeholder="Nome completo"
+          value={ name }
           onChange={ ({ target }) => setName(target.value) }
         />
-        <input type="number" placeholder="Idade" />
-        <input type="text" placeholder="Cidade" />
+        <input
+          type="number"
+          placeholder="Idade"
+          value={ age }
+          onChange={ ({ target }) => setAge(target.value) }
+        />
+        <input
+          type="text"
+          placeholder="Cidade"
+          value={ city }
+          onChange={ ({ target }) => setCity(target.value) }
+        />
         <label htmlFor="module">
           <h2>Módulo</h2>
-          <input type="radio" id="fundaments" name="module" value="fundaments" />
+          <input
+            type="radio"
+            id="fundaments"
+            name="module"
+            value="fundaments"
+            checked={ module === 'fundaments' }
+            onChange={ ({ target }) => setModule(target.value) }
+          />
           <label htmlFor="fundaments">Fundamentos</label>
-          <input type="radio" id="frontend" name="module" value="frontend" />
+          <input
+            type="radio"
+            id="frontend"
+            name="module"
+            value="frontend"
+            checked={ module === 'frontend' }
+            onChange={ ({ target }) => setModule(target.value) }
+          />
           <label htmlFor="frontend">Frontend</label>
-          <input type="radio" id="backend" name="module" value="backend" />
+          <input
+            type="radio"
+            id="backend"
+            name="module"
+            value="backend"
+            checked={ module === 'backend' }
+            onChange={ ({ target }) => setModule(target.value) }
+          />
           <label htmlFor="backend">Backend</label>
-          <input type="radio" id="science" name="module" value="science" />
+          <input
+            type="radio"
+            id="science"
+            name="module"
+            value="science"
+            checked={ module === 'science' }
+            onChange={ ({ target }) => setModule(target.value) }
+          />
           <label htmlFor="science">Ciência da Computação</label>
         </label>
         <button type="button">Enviar</button>
