92da908b684eaa482a4357b29a8a4e68de19cc17
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-07 11:49:14 -0300

    ✨ feat: creating global constants

--- 2-front-end/9-context-api-e-react-hooks/dia-1-react-hooks-usestate-useeffect/trybe-mail/src/App.jsx
+++ 2-front-end/9-context-api-e-react-hooks/dia-1-react-hooks-usestate-useeffect/trybe-mail/src/App.jsx
@@ -1,27 +1,32 @@
+import { useState } from 'react';
+
 import './App.css'
 import Emails from './components/Emails'
 import emailsList from "./data/emailsList";
-import { useState } from 'react';
 
 function App() {
-  const setMessageStatus = (emailId, newStatus) => {
-    const updatedMessages = emailsList.map((currEmail) => {
-      if (currEmail.id === emailId) {
-        return { ...currEmail, status: newStatus };
-      }
-      return currEmail;
-    });
-
-    setMessages(updatedMessages);
-  }
-
   const [messages, setMessages] = useState(emailsList)
+
+  // const setMessageStatus = (emailId, newStatus) => {
+  //   const updatedMessages = emailsList.map((currEmail) => {
+  //     if (currEmail.id === emailId) {
+  //       return { ...currEmail, status: newStatus };
+  //     }
+  //     return currEmail;
+  //   });
+
+  //   setMessages(updatedMessages);
+  // }
+
   return (
     <div className="App">
-      <h1>TrybeMail</h1>
+      <header>
+        <h1>TrybeMail</h1>
+      </header>
+
       <Emails
         emailsList={messages}
-         />
+      />
     </div>
   )
 }
--- 2-front-end/9-context-api-e-react-hooks/dia-1-react-hooks-usestate-useeffect/trybe-mail/src/components/Emails.jsx
+++ 2-front-end/9-context-api-e-react-hooks/dia-1-react-hooks-usestate-useeffect/trybe-mail/src/components/Emails.jsx
@@ -4,10 +4,10 @@ const Emails = ({ emailsList, setMessageStatus }) => {
       {emailsList.map((email) =>
         <li key={email.id}>
           <h3>{email.title}</h3>
-          <div>
+          {/* <div>
             <button onClick={}>Marcar como lido</button>
             <button onClick={}>Marcar como não lido</button>
-          </div>
+          </div> */}
         </li>
       )}
     </ul>
