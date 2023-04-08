4011a834893446821a73b9ed92a2590453d8609a
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-07 21:42:02 -0300

    ✨ feat: allMessages

--- 2-front-end/9-context-api-e-react-hooks/dia-1-react-hooks-usestate-useeffect/trybe-mail/src/App.jsx
+++ 2-front-end/9-context-api-e-react-hooks/dia-1-react-hooks-usestate-useeffect/trybe-mail/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import './App.css'
 import Emails from './components/Emails'
 import emailsList from "./data/emailsList";
+import { READ, UNREAD } from './constants';
+import AllButtons from './components/AllButtons';
 
 function App() {
   const [messages, setMessages] = useState(emailsList)
@@ -18,18 +20,34 @@ function App() {
     setMessages(updatedMessages);
   };
 
-  return (
-    <div className="App">
-      <header>
-        <h1>TrybeMail</h1>
-      </header>
+  const markAllAsRead = () => {
+    const allAsRead = messages.map((message) => ({ ...message, status: READ }))
+    setMessages(allAsRead);
+  }
 
-      <Emails
-        emailsList={messages}
-        setMessageStatus={setMessageStatus}
-      />
-    </div>
-  )
+  const markAllAsUnread = () => {
+    const allAsUnread = messages.map((message) => ({ ...message, status: UNREAD }))
+    setMessages(allAsUnread);
+  }
+
+  useEffect(() => {
+    const isAllRead = messages.every((message) => message.status === READ)
+
+    if(isAllRead) alert('Parabéns! Você leu todas suas mensagens!')
+}, [messages]);
+
+return (
+  <div className="App">
+    <header>
+      <h1>TrybeMail</h1>
+    </header>
+    <AllButtons markAllAsRead={markAllAsRead} markAllAsUnread={markAllAsUnread} />
+    <Emails
+      emailsList={messages}
+      setMessageStatus={setMessageStatus}
+    />
+  </div>
+)
 }
 
 export default App
