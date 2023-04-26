09146fe778fd5c91fd5c6c1225799202d2d31672
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-26 11:10:56 -0300

    ✨ feat: finishing app

--- 2-front-end/9-context-api-e-react-hooks/dia-2-context-api/context-api-exercise/src/App.jsx
+++ 2-front-end/9-context-api-e-react-hooks/dia-2-context-api/context-api-exercise/src/App.jsx
@@ -3,11 +3,16 @@ import ThemeContext from './context/ThemeContext';
 import Header from './components/Header';
 import Image from './components/Image';
 import Footer from './components/Footer';
+import { useState } from 'react';
 
 export default function App() {
+  const [themeColor, setThemeColor] = useState('dark');
+  function toggleTheme() {
+    setThemeColor(themeColor === 'dark' ? 'light' : 'dark');
+  }
   return (
-    <ThemeContext.Provider value={{ color: 'dark' }}>
-      <div>
+    <ThemeContext.Provider value={{ color: themeColor, toggleTheme }}>
+      <div className={`app ${themeColor}`}>
         <Header />
         <Image />
         <Footer />
