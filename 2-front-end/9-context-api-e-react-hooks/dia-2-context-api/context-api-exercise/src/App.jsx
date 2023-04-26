abbaae02baaba5fe5b0a0122d5ed8d1656f7c96e
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-26 11:13:34 -0300

    ✨ feat: refactor, create themeProvider component

--- 2-front-end/9-context-api-e-react-hooks/dia-2-context-api/context-api-exercise/src/App.jsx
+++ 2-front-end/9-context-api-e-react-hooks/dia-2-context-api/context-api-exercise/src/App.jsx
@@ -1,22 +1,14 @@
-
-import ThemeContext from './context/ThemeContext';
+import ThemeProvider from './context/ThemeProvider';
 import Header from './components/Header';
-import Image from './components/Image';
 import Footer from './components/Footer';
-import { useState } from 'react';
+import Image from './components/Image';
 
 export default function App() {
-  const [themeColor, setThemeColor] = useState('dark');
-  function toggleTheme() {
-    setThemeColor(themeColor === 'dark' ? 'light' : 'dark');
-  }
   return (
-    <ThemeContext.Provider value={{ color: themeColor, toggleTheme }}>
-      <div className={`app ${themeColor}`}>
-        <Header />
-        <Image />
-        <Footer />
-      </div>
-    </ThemeContext.Provider>
+    <ThemeProvider>
+      <Header />
+      <Image />
+      <Footer />
+    </ThemeProvider>
   );
 }
