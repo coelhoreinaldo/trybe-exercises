f4094c025742d59d82148c2ee9e0ada171d59c36
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-09 18:24:39 -0300

    ✨ feat: exercise 6C

--- 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/simpsons/simpsons.js
+++ 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/simpsons/simpsons.js
@@ -26,3 +26,16 @@ const readCharacterInfoById = async (id) => {
 }
 
 // readCharacterInfoById(7)
+
+const removeCharactersFromJson = async () => {
+  try {
+    const response = JSON.parse(await fs.readFile('./simpsons.json'))
+    const data = response.filter((id) => id !== 10 && id !== 6)
+    await fs.writeFile('./simpsons.json', JSON.stringify(data))
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+removeCharactersFromJson()
