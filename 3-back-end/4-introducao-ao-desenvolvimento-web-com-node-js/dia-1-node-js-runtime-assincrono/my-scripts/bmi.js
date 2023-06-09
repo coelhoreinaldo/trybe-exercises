b421a64af7f9398c5f17deca145beccfd2fe2b9a
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-09 17:47:12 -0300

    ✨ feat: exercise 5, imc

--- 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/my-scripts/bmi.js
+++ 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/my-scripts/bmi.js
@@ -1,8 +1,39 @@
 const readline = require("readline-sync")
 
-const weight = readline.questionInt('What\'s your weight?');
-const height = readline.questionFloat('What\'s your height?');
+const weight = readline.questionFloat('What\'s your weight? (kg) ');
+const height = readline.questionFloat('What\'s your height? (cm) ');
 
-const calculateBMI = () => console.log((weight / (height * height)).toFixed(2));
+const calculateBMI = () => {
+  const heightInCm = height / 100;
+  const bmi = (weight / (heightInCm * heightInCm)).toFixed(2)
+  console.log(bmi)
+  return bmi
+};
 
-calculateBMI()
+const main = () => {
+  const bmi = calculateBMI();
+  if (bmi < 18.5) {
+    console.log('Abaixo do peso (magreza)')
+    return 'Abaixo do peso (magreza)'
+  }
+  if (bmi < 24.9) {
+    console.log('Peso normal')
+    return 'Peso normal'
+  }
+  if (bmi < 29.9) {
+    console.log('Acima do peso (sobrepeso)')
+    return 'Acima do peso (sobrepeso)'
+  }
+  if (bmi < 34.9) {
+    console.log('Obesidade grau I')
+    return 'Obesidade grau I'
+  }
+  if (bmi < 39.9) {
+    console.log('Obesidade grau II')
+    return 'Obesidade grau II'
+  }
+  console.log('Obesidade graus III e IV')
+  return 'Obesidade graus III e IV'
+}
+
+main();
