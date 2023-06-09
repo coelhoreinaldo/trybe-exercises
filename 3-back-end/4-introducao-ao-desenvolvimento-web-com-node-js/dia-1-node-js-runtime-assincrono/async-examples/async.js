de5917a07952401d9349bcc05745cfa6079e9ddd
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-09 15:24:48 -0300

    ✨ feat: async

---
+++ 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/async-examples/async.js
@@ -0,0 +1,20 @@
+function dividirNumeros(num1, num2) {
+  const promise = new Promise((resolve, reject) => {
+    if (num2 == 0)
+      reject(new Error("Não pode ser feito uma divisão por zero"));
+
+    const resultado = num1 / num2;
+    resolve(resultado)
+  });
+
+  return promise;
+}
+
+// dividirNumeros(2, 1)
+//   .then(result => console.log(`sucesso: ${result}`))
+//   .catch(err => console.log(`erro: ${err.message}`));
+
+const doSomething = async () => {
+  console.log(await dividirNumeros(2, 2));
+};
+
