3ec0be5c845280d34af290a7352b3624a3d180a4
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-15 16:32:33 -0300

    :sparkles: feat: create createdAt middleware

--- 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/ecotourism-app/src/app.js
+++ 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/ecotourism-app/src/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const validateActivityName = require('./middlewares/validateActivityName');
 const validateActivityPrice = require('./middlewares/validateActivityPrice');
+const validateActivityDescription = require('./middlewares/validateActivityDescription');
+const validateActivityCreatedAt = require('./middlewares/validateActivityCreatedAt');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', validateActivityName, validateActivityPrice, (req, res) => {
-  res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
-});
+app.post('/activities',
+  validateActivityName,
+  validateActivityPrice,
+  validateActivityDescription,
+  validateActivityCreatedAt, (req, res) => {
+    res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
+  });
 
 module.exports = app;
