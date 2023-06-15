db317c000606df1e3604c2d29ef52c5cbd8d9e44
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-15 09:52:12 -0300

    ✨ feat: create middlewares

--- 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/soccer-team-4-4/src/app.js
+++ 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/soccer-team-4-4/src/app.js
@@ -24,23 +24,26 @@ app.get('/teams/:id', (req, res) => {
   }
 });
 
-app.post('/teams', (req, res) => {
+const validateTeam = (req, res, next) => {
   const requiredProperties = ['nome', 'sigla'];
   if (requiredProperties.every((property) => property in req.body)) {
-    const team = { id: nextId, ...req.body };
-    teams.push(team);
-    nextId += 1;
-    res.status(201).json(team);
+    next(); // Chama o próximo middleware
   } else {
-    res.sendStatus(400);
+    res.sendStatus(400); // Ou já responde avisando que deu errado
   }
+};
+
+app.post('/teams', validateTeam, (req, res) => {
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
 });
 
-app.put('/teams/:id', (req, res) => {
+app.put('/teams/:id', validateTeam, (req, res) => {
   const id = Number(req.params.id);
-  const requiredProperties = ['nome', 'sigla'];
   const team = teams.find(t => t.id === id);
-  if (team && requiredProperties.every((property) => property in req.body)) {
+  if (team) {
     const index = teams.indexOf(team);
     const updated = { id, ...req.body };
     teams.splice(index, 1, updated);
