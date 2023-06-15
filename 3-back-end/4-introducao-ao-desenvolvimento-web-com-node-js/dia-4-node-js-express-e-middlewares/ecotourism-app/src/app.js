334de36ae52c502de792924a86ba0575d1046e58
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-15 16:20:52 -0300

    :sparkles: feat: create middlewares for name and price

--- 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/ecotourism-app/src/app.js
+++ 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/ecotourism-app/src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const validateActivityName = require('./middlewares/validateActivityName');
+const validateActivityPrice = require('./middlewares/validateActivityPrice');
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/activities', validateActivityName, validateActivityPrice, (req, res) => {
+  res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
+});
 
 module.exports = app;
