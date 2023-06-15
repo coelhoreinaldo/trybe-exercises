460a26feda53401be3b47bfe766655ff64327b54
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-15 15:17:45 -0300

    :sparkles: feat: use router

--- 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/soccer-team-4-4/src/app.js
+++ 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/soccer-team-4-4/src/app.js
@@ -6,10 +6,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
-const validateTeam = require('./middlewares/validateTeam');
-const existingId = require('./middlewares/existingId');
-const teams = require('./utils/teams');
-const apiCredentials = require('./middlewares/apiCredentials');
+const teamsRouter = require('./routes/teamsRouter');
 
 const app = express();
 
@@ -19,54 +16,13 @@ const limiter = rateLimit({
   message: 'Muitas requisições originadas desta IP', // mensagem personalizada quando atinge o limit rate
 });
 
-let nextId = 3;
-
+app.use(morgan('dev'));
 app.use(limiter);
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
-app.use(apiCredentials);
-app.use(morgan('dev'));
 
-app.get('/teams', (req, res) => res.json(teams));
-
-app.get('/teams/:id', existingId, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-  res.json(team);
-});
-
-app.post('/teams', validateTeam, (req, res) => {
-  if (
-    // confere se a sigla proposta está inclusa nos times autorizados
-    !req.teams.teams.includes(req.body.sigla)
-    // confere se já não existe um time com essa sigla
-    && teams.every((t) => t.sigla !== req.body.sigla)
-  ) {
-    return res.status(422).json({ message: 'Já existe um time com essa sigla' });
-  }
-  const team = { id: nextId, ...req.body };
-  teams.push(team);
-  nextId += 1;
-  res.status(201).json(team);
-});
-
-app.put('/teams/:id', existingId, validateTeam, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-  const index = teams.indexOf(team);
-  const updated = { id, ...req.body };
-  teams.splice(index, 1, updated);
-  res.status(201).json(updated);
-});
-
-app.delete('/teams/:id', existingId, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-  const index = teams.indexOf(team);
-  teams.splice(index, 1);
-  res.sendStatus(204);
-});
+app.use('/teams', teamsRouter);
 
 app.use((err, _req, _res, next) => {
   console.error(err.stack);
--- 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/soccer-team-4-4/src/middlewares/existingId.js
+++ 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/soccer-team-4-4/src/middlewares/existingId.js
@@ -1,12 +1,12 @@
-const teams = require("../utils/teams");
+const teams = require('../utils/teams');
 
 const existingId = (req, res, next) => {
   const id = Number(req.params.id);
-  const team = teams.find(t => t.id === id);
+  const team = teams.find((t) => t.id === id);
   if (team) {
     return next();
   }
   res.status(404).send({ message: 'Time não encontrado!' });
-}
+};
 
 module.exports = existingId;
