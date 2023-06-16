88e87556c695acf4d8dd215a53f56bec2d3a92e4
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-16 11:08:43 -0300

    :sparkles: feat: creat insert in db

--- 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-5-node-js-express-e-my-sql/trybecash-api/src/app.js
+++ 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-5-node-js-express-e-my-sql/trybecash-api/src/app.js
@@ -1,9 +1,9 @@
-// src/app.js
-
 const express = require('express');
+const peopleRoutes = require('./routes/peopleRoutes');
 
 const app = express();
 
 app.use(express.json());
+app.use('/people', peopleRoutes);
 
 module.exports = app;
