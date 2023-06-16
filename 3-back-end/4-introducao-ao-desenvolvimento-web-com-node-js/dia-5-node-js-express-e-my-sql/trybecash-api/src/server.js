e32aca68b4ca35382ee773371b0c4594bd16eb55
coelhoreinaldo <scoelhor7@gmail.com>
2023-06-16 19:47:45 -0300

    🔧 fix: localhost 300

--- 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-5-node-js-express-e-my-sql/trybecash-api/src/server.js
+++ 3-back-end/4-introducao-ao-desenvolvimento-web-com-node-js/dia-5-node-js-express-e-my-sql/trybecash-api/src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = 3001;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
