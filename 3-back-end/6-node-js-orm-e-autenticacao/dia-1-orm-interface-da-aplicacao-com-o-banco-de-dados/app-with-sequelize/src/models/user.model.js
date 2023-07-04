a7c2254cd17bf4d31130575452df971b725fc123
coelhoreinaldo <scoelhor7@gmail.com>
2023-07-04 11:34:06 -0300

    ✨ feat: seeders

--- 3-back-end/6-node-js-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/models/user.model.js
+++ 3-back-end/6-node-js-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/models/user.model.js
@@ -1,21 +1,13 @@
-const UserModel = (sequelize, DataTypes) => {
+src / models / user.model.js
+
+const User = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
     phoneNum: DataTypes.STRING,
   });
-  (async () => {
-    await sequelize.sync({ force: true });
-    // As funções vão aqui
-    const sara = User.build({
-      fullName: 'Sara Silva Santos',
-      email: 'sara.ss@trybe.com',
-    });
 
-    console.log(sara instanceof User); // true
-    console.log(sara.fullName); // "Sara Silva Santos"
-  })();
   return User;
 };
 
-module.exports = UserModel;
+module.exports = User;
