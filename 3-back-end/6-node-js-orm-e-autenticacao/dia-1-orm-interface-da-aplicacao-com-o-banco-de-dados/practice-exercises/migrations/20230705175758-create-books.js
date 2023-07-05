96e444a12365d34a649de0bde8c443a8d14e40cc
coelhoreinaldo <scoelhor7@gmail.com>
2023-07-05 15:52:16 -0300

    ✨ feat: exercise 1

---
+++ 3-back-end/6-node-js-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/practice-exercises/migrations/20230705175758-create-books.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      pageQuantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('books');
+  }
+};
