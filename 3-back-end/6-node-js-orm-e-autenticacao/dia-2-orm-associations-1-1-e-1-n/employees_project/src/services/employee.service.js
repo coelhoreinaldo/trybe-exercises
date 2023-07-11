c44d8343249cf4d829831b30478a7b1ba19f88fa
coelhoreinaldo <scoelhor7@gmail.com>
2023-07-11 09:45:37 -0300

    ✨ feat: unmanaged transactions

--- 3-back-end/6-node-js-orm-e-autenticacao/dia-2-orm-associations-1-1-e-1-n/employees_project/src/services/employee.service.js
+++ 3-back-end/6-node-js-orm-e-autenticacao/dia-2-orm-associations-1-1-e-1-n/employees_project/src/services/employee.service.js
@@ -1,4 +1,10 @@
 const { Address, Employee } = require('../models/');
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+const env = process.env.NODE_ENV || 'development';
+
+const sequelize = new Sequelize(config[env]);
+
 
 const getAll = async () => {
   const users = await Employee.findAll({
@@ -16,10 +22,30 @@ const getById = async (id) => {
 }
 
 const insert = async ({ firstName, lastName, age, city, street, number }) => {
-  const employee = await Employee.create({ firstName, lastName, age });
+  const t = await sequelize.transaction();
+  try {
+    // Depois executamos as operações
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
 
-  await Address.create({ city, street, number, employeeId: employee.id });
-  return employee;
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t },
+    );
+
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await t.commit();
+    return employee;
+  } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    await t.rollback();
+    console.log(e);
+    throw e; // Jogamos o erro para a controller tratar
+  }
 };
 
 module.exports = {
