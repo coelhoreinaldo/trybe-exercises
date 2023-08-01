39f2c3d9b2b32d18834c1ea7a0d83c2726f36ee4
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-01 09:57:27 -0300

    ✨ feat: type annotations

--- 3-back-end/8-introducao-a-typescript/dia-1-aprendendo-typescript/example-1/hello.ts
+++ 3-back-end/8-introducao-a-typescript/dia-1-aprendendo-typescript/example-1/hello.ts
@@ -1,1 +1,16 @@
 console.log('Hello, TypeScript!');
+
+// type annotations
+
+const firstName: string = "Joel";
+const age: number = 38;
+const brazilian: boolean = false;
+
+function printPersonalInfo(data: { name: string; birthYear: number }) {
+  console.log(`${data.name} was born in ${data.birthYear}.`);
+}
+printPersonalInfo({ name: 'Rogerinho', birthYear: 1978 });
+
+const evenNumbers: number[] = [2, 4, 6];
+const vowel: string[] = ['a', 'e', 'i', 'o', 'u'];
+const booleanValues: boolean[] = [true, false];
