f9afc6a03740a2d105e549ccb847555a636857d8
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-01 10:02:04 -0300

    ✨ feat: union types

--- 3-back-end/8-introducao-a-typescript/dia-1-aprendendo-typescript/example-1/hello.ts
+++ 3-back-end/8-introducao-a-typescript/dia-1-aprendendo-typescript/example-1/hello.ts
@@ -14,3 +14,18 @@ printPersonalInfo({ name: 'Rogerinho', birthYear: 1978 });
 const evenNumbers: number[] = [2, 4, 6];
 const vowel: string[] = ['a', 'e', 'i', 'o', 'u'];
 const booleanValues: boolean[] = [true, false];
+
+evenNumbers.push(8) // Funciona
+// evenNumbers.push('8') // Erro
+
+// union types
+
+function printId(id: number | string) {
+  if (typeof id === "string") {
+    return console.log(id.toUpperCase());
+  }
+  return console.log(id);
+}
+
+printId(101);
+printId("202");
