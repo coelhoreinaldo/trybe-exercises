c313cff6c2b175cdb07075ef5ff132acaa73497e
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-16 08:45:26 -0300

    ✨ feat: create student class answer sheets

--- 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-1-introducao-a-orientacao-a-objetos/exercise-4/index.ts
+++ 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-1-introducao-a-orientacao-a-objetos/exercise-4/index.ts
@@ -1,15 +1,27 @@
 class Student {
-  private _matricula: string;
-  private _nome: string;
-  private _provasNotas: number[];
-  private _trabalhosNotas: number[];
+  private _studentId: string;
+  private _name: string;
+  private _examsGrades: number[];
+  private _assignmentsGrades: number[];
 
   constructor(
-    matricula:string, nome:string, provasNotas:number[], trabalhosNotas:number[]
+    studentId:string, name:string, examsGrades:number[], assignmentsGrades:number[]
   ){
-    this._matricula = matricula;
-    this._nome = nome;
-    this._provasNotas = provasNotas;
-    this._trabalhosNotas = trabalhosNotas;
+    this._studentId = studentId;
+    this._name = name;
+    this._examsGrades = examsGrades;
+    this._assignmentsGrades = assignmentsGrades;
+  }
+
+  get studentId():string { return this._studentId }
+  get name():string { return this._name }
+  get examsGrades():number[] { return this._examsGrades }
+  get assignmentsGrades():number[] { return this._assignmentsGrades }
+
+  calculateGrades(){
+    const examsTotal = this._examsGrades.reduce((acc, curr) => acc + curr, 0)
+    const assignmentsTotal = this._assignmentsGrades.reduce((acc, curr) => acc + curr, 0)
+
+    return examsTotal + assignmentsTotal;
   }
 }
