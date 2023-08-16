2e58e273d52adf6a3dde3001c90d715fb47a13e1
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-16 12:01:06 -0300

    ✨ feat: interface

--- 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-2-heranca-e-composicao/exercises-1/Animal.ts
+++ 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-2-heranca-e-composicao/exercises-1/Animal.ts
@@ -1,53 +1,54 @@
-class Animal {
-  constructor(public name: string, protected birthDate: Date) { }
+// class Animal {
+//   constructor(public name: string, protected birthDate: Date) { }
 
-  get age() {
-    const timeDiff = Math.abs(
-      Date.now() -
-      new Date(this.birthDate).getTime()
-    );
+//   get age() {
+//     const timeDiff = Math.abs(
+//       Date.now() -
+//       new Date(this.birthDate).getTime()
+//     );
 
-    return Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
-  }
-}
+//     return Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+//   }
+// }
 
-class Mammal extends Animal {
-  walk() {
-    console.log(`${this.name} está andando!`);
-  }
-}
+// class Mammal extends Animal {
+//   walk() {
+//     console.log(`${this.name} está andando!`);
+//   }
+// }
 
-const tiger = new Mammal(
-  'Tigre',
-  new Date(Date.parse('May 03, 2020')),
-);
+// const tiger = new Mammal(
+//   'Tigre',
+//   new Date(Date.parse('May 03, 2020')),
+// );
 
-const main = (animal: Animal) => {
-  console.log(animal.age);
-}
+// const main = (animal: Animal) => {
+//   console.log(animal.age);
+// }
 
-// main(tiger);
-// tiger.walk();
+// // main(tiger);
+// // tiger.walk();
 
-class Bird extends Animal {
-  fly() {
-    console.log(`${this.name} está voando!`);
-  }
+// class Bird extends Animal {
+//   fly() {
+//     console.log(`${this.name} está voando!`);
+//   }
 
-  showBirthDate(){
-    console.log(this.birthDate);
-  }
-}
+//   showBirthDate(){
+//     console.log(this.birthDate);
+//   }
+// }
 
-const parrot = new Bird(
-  'Papagaio',
-  new Date(Date.parse('Jun 07, 2017')),
-);
+// const parrot = new Bird(
+//   'Papagaio',
+//   new Date(Date.parse('Jun 07, 2017')),
+// );
 
 // console.log(parrot.age);
 // parrot.fly();
-parrot.showBirthDate();
+// parrot.showBirthDate();
 
+// Super 
 
 // class Animal {
 //   constructor(protected birthDate: Date) { }
@@ -57,3 +58,41 @@ parrot.showBirthDate();
 //     super(new Date());
 //   }
 // }
+
+// Interfaces
+
+interface Animal {
+  name: string;
+  age: number;
+
+  getBirthDate(): Date;
+}
+
+class Bird implements Animal {
+  constructor(
+    public name: string,
+    private birthDate: Date) {}
+
+  get age() {
+    var timeDiff = Math.abs(Date.now() - new Date(this.birthDate).getTime());
+    return Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+  }
+
+  getBirthDate() { return this.birthDate; }
+
+  fly() { console.log(`${this.name} está voando!`); }
+}
+
+const parrot = new Bird(
+  'Papagaio',
+  new Date(Date.parse('Aug 16, 2015')),
+);
+
+console.log(parrot.age);
+parrot.fly();
+
+/*
+Saída (código executado em Mar/2022):
+6
+Papagaio está voando!
+*/
