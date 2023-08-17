c9400442970c064fc3c6fa22851117b6666e1b01
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-17 11:17:35 -0300

    ✨ feat: exercise 5, teacher

--- 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-2-heranca-e-composicao/exercises-2/Employee.ts
+++ 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-2-heranca-e-composicao/exercises-2/Employee.ts
@@ -1,5 +1,5 @@
 export default interface Employee {
-  registration: number;
+  registration: string;
   salary: number;
   admissionDate: Date;
   generateRegistration(): string;
--- 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-2-heranca-e-composicao/exercises-2/index.ts
+++ 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-2-heranca-e-composicao/exercises-2/index.ts
@@ -1,6 +1,7 @@
 import Person from './Person';
 import Student from './Student';
 import Subject from './Subject';
+import Teacher from './Teacher';
 
 // Exercise 1
 
@@ -43,8 +44,18 @@ tamires.assignmentsGrades = [47, 42];
 
 const math = new Subject('matemática')
 const philosophy = new Subject('filosofia')
-const story = new Subject('história')
+const history = new Subject('história')
 
-console.log(math);
-console.log(philosophy);
-console.log(story);
+// console.log(math);
+// console.log(philosophy);
+// console.log(history);
+
+// Exercise 5 
+
+const marta = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
+const joao = new Teacher('João Antônio da Costa', new Date('1982/04/21'), 2000, history);
+const lucio = new Teacher('Lucio Teixeira', new Date('1986/01/29'), 2000, philosophy);
+
+console.log(marta);
+console.log(joao);
+console.log(lucio);
