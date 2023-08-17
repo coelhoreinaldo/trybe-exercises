ed65156e9c23363437696ff61d5295b96a9c5ff4
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-17 15:07:39 -0300

    ✨ feat: create localdb model

--- 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-3-polimorfismo/to-fix-2/Character.ts
+++ 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-3-polimorfismo/to-fix-2/Character.ts
@@ -10,9 +10,44 @@ interface DbCharacter extends Character {
 const db: DbCharacter[] = [];
 
 interface IModel {
+  create(character: Character): Promise<Character>;
   getAll(): Promise<DbCharacter[]>;
   getById(id: number): Promise<DbCharacter>;
-  create(character: Character): Promise<Character>;
   update(id: number, character:Character): Promise<Character>;
   remove(id: number): Promise<boolean>;
+}
+
+class LocalDbModel implements IModel {
+  create = async (character: Character) => {
+    const lastId = db.length > 0 ? db[db.length - 1].id : 0;
+    const newCharacter = { id: lastId + 1, ...character };
+    db.push(newCharacter);
+    return newCharacter;
+  };
+
+  findIndexById = (id: number) => {
+    const index = db.findIndex((character) => character.id === id);
+    if (index < 0) throw new Error('Character not found');
+    return index;
+  };
+
+  update = async (id: number, character: Character) => {
+    const indexToUpdate = this.findIndexById(id);
+    db[indexToUpdate] = { ...db[indexToUpdate], ...character };
+    return db[indexToUpdate];
+  };
+
+  remove = async (id: number) => {
+    const indexToDelete = this.findIndexById(id);
+    const deletedItem = db.splice(indexToDelete, 1);
+    if (deletedItem.length > 0) return true;
+    return false;
+  };
+
+  getAll = async () => db;
+
+  getById = async (id: number) => {
+    const indexToGet = this.findIndexById(id);
+    return db[indexToGet];
+  };
 }
