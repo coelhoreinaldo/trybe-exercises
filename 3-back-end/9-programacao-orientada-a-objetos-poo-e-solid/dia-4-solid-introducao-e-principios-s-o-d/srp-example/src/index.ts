0b9b168d662805f3e8f68fb9654fbc754a8dcdbc
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-18 14:42:11 -0300

    🧪 test: getlettergrades test

--- 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-4-solid-introducao-e-principios-s-o-d/srp-example/src/index.ts
+++ 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-4-solid-introducao-e-principios-s-o-d/srp-example/src/index.ts
@@ -12,21 +12,30 @@ type Student = {
 };
 
 /* "Converter" */
-const percentageGradesIntoLetters = ({ name: studentName, disciplines }: Student):
-  { name: string, disciplines: Discipline[]} => ({
-  name: studentName,
-  disciplines: disciplines.map(({ name, grade }) => {
-    let letterGrade;
 
-    if (grade >= 0.9) letterGrade = 'A';
-    else if (grade >= 0.8) letterGrade = 'B';
-    else if (grade >= 0.7) letterGrade = 'C';
-    else if (grade >= 0.6) letterGrade = 'D';
-    else if (grade >= 0.1) letterGrade = 'E';
-    else letterGrade = 'F';
+/* Apoio para a função `getGradeLetter` */
+const GRADE_DICT = {
+  numbers: [0.9, 0.8, 0.7, 0.6, 0.1],
+  letters: ['A', 'B', 'C', 'D', 'E'],
+};
 
-    return { name, grade, letterGrade };
-  }),
+const getGradeLetter = (gradeNumber: number): string => {
+  const gradeNumbers = GRADE_DICT.numbers;
+  const gradeLetters = GRADE_DICT.letters;
+  for (let i = 0; i < gradeNumbers.length; i += 1) {
+    if (gradeNumber >= gradeNumbers[i]) return gradeLetters[i];
+  }
+  return 'F';
+};
+
+const getLetterGrades = (discipline: Discipline): Discipline => ({
+  ...discipline,
+  letterGrade: getGradeLetter(discipline.grade),
+});
+
+const percentageGradesIntoLetters = (student: Student): Student => ({
+  ...student,
+  disciplines: student.disciplines.map(getLetterGrades),
 });
 
 /* "Determinar" */
@@ -55,6 +64,7 @@ function setApproved(students: Student[]): void {
   possa testá-las
 */
 export {
+  getLetterGrades,
   percentageGradesIntoLetters,
   approvedStudents,
   updateApprovalData,
