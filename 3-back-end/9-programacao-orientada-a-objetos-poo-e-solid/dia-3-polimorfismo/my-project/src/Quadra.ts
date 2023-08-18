2362a6b4914158e71c99a98a40639bd4c6aa1e2b
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-18 10:04:52 -0300

    ✨ feat: exercise 2, create abstract quadra class

--- 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-3-polimorfismo/my-project/src/Quadra.ts
+++ 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-3-polimorfismo/my-project/src/Quadra.ts
@@ -1,4 +1,8 @@
+import IAgenda from "./interfaces/iAgenda";
+
 // src/Quadra.ts
-class Quadra { }
+abstract class Quadra { 
+  protected abstract reservar<T>(date: Date):IAgenda<T>;
+}
 
 export default Quadra;
