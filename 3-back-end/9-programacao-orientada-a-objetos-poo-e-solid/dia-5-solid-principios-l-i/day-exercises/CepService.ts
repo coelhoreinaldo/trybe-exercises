70c0e49642fb131d61a847816929f669f9ab50ee
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-21 13:41:06 -0300

    🧱 build: setup

---
+++ 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-5-solid-principios-l-i/day-exercises/CepService.ts
@@ -0,0 +1,19 @@
+import FooCepAPI from './FooCepAPI';
+
+class CepService {
+  private readonly cepApi: FooCepAPI;
+
+  constructor() {
+    this.cepApi = new FooCepAPI();
+  }
+
+  addressByCep(cep: string, num: number) {
+    return this.cepApi.getAddressByCEP(cep, num);
+  }
+
+  cepByAddress(address: string, num: number) {
+    return this.cepApi.getCepByAddress(address, num);
+  }
+}
+
+export default CepService;
---
+++ 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-5-solid-principios-l-i/day-exercises/index.ts
@@ -0,0 +1,18 @@
+import CepService from './CepService';
+
+async function main() {
+  const cepSvc = new CepService();
+
+  console.log(
+    'get address by cep', 
+    '->', 
+    await cepSvc.addressByCep('xx.xxx-xx', 10),
+  );
+  console.log(
+    'get cep by address', 
+    '->', 
+    await cepSvc.cepByAddress('street foo, between bar and baz', 10),
+  );
+}
+
+main();
