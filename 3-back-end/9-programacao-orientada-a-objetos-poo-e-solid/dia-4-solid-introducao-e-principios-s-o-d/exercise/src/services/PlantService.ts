00016a62ceb94fc192f94b272aa095ed293fded3
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-21 09:17:07 -0300

    🔧 fix: service refactor

--- 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-4-solid-introducao-e-principios-s-o-d/exercise/src/services/PlantService.ts
+++ 3-back-end/9-programacao-orientada-a-objetos-poo-e-solid/dia-4-solid-introducao-e-principios-s-o-d/exercise/src/services/PlantService.ts
@@ -1,67 +1,52 @@
-import fs from 'fs/promises';
-import path from 'path';
-import HttpException from '../exceptions/HttpException';
-import { INewPlant, IPlant } from '../interfaces/IPlant';
-import IOpsInfo from '../models/interfaces/IOpsInfo';
+import { INewPlant, IPlant } from '../interfaces';
+import { IService } from './interfaces';
+import { IModel } from '../models/interfaces';
+import PlantValidate from './validations/PlantValidate';
+import { NotFoundException } from '../exceptions';
 
-class PlantService {
-  private readonly plantsFile = path.join(__dirname, '..', 'models', 'database', 'plantsData.json');
+class PlantService implements IService<IPlant, INewPlant> {
+  private readonly model: IModel<IPlant>;
 
-  private readonly opsFile = path.join(__dirname, '..', 'models', 'database', 'opsInfo.json');
-
-  private async updateOpsInfo(incrementAmount = 1): Promise<number> {
-    const dataRaw = await fs.readFile(this.opsFile, { encoding: 'utf8' });
-    const opsInfo: IOpsInfo = JSON.parse(dataRaw);
-    opsInfo.createdPlants += incrementAmount;
-
-    await fs.writeFile(this.opsFile, JSON.stringify(opsInfo, null, 2));
-
-    return opsInfo.createdPlants;
+  constructor(model: IModel<IPlant>) {
+    this.model = model;
   }
 
   public async getAll(): Promise<IPlant[]> {
-    const dataRaw = await fs.readFile(this.plantsFile, { encoding: 'utf8' });
-    const plants: IPlant[] = JSON.parse(dataRaw);
+    const plants = await this.model.getAll();
     return plants;
   }
 
   public async create(plant: INewPlant): Promise<IPlant> {
-    const {
-      breed,
-      needsSun,
-      origin,
-      size,
-    } = plant;
+    PlantValidate.validateAttributes(plant);
 
-    if (typeof breed !== 'string') {
-      throw new HttpException(400, 'Attribute "breed" must be string.');
-    }
-
-    if (typeof needsSun !== 'boolean') {
-      throw new HttpException(400, 'Attribute "needsSun" must be boolean.');
-    }
-
-    if (typeof origin !== 'string') {
-      throw new HttpException(400, 'Attribute "origin" must be string.');
-    }
-
-    if (typeof size !== 'number') {
-      throw new HttpException(400, 'Attribute "size" must be number.');
-    }
-
+    const { needsSun, size, origin } = plant;
     const waterFrequency = needsSun
       ? size * 0.77 + (origin === 'Brazil' ? 8 : 7)
       : (size / 2) * 1.33 + (origin === 'Brazil' ? 8 : 7);
 
-    const dataRaw = await fs.readFile(this.plantsFile, { encoding: 'utf8' });
-    const plants: IPlant[] = JSON.parse(dataRaw);
-
-    const newPlantId = await this.updateOpsInfo(1);
-    const newPlant = { id: newPlantId, ...plant, waterFrequency };
-    plants.push(newPlant);
-
-    await fs.writeFile(this.plantsFile, JSON.stringify(plants, null, 2));
+    const newPlant = await this.model.create({ ...plant, waterFrequency });
     return newPlant;
+  }
+
+  public async getById(id: string): Promise<IPlant> {
+    const plant = await this.model.getById(id);
+    if (!plant) throw new NotFoundException('Plant not Found!');
+    return plant;
+  }
+
+  public async update(id: string, plant: Omit<IPlant, 'id'>): Promise<IPlant> {
+    const plantExists = await this.model.getById(id);
+    if (!plantExists) throw new NotFoundException('Plant not Found!');
+
+    PlantValidate.validateAttributes(plant);
+
+    const editedPlant = await this.model.update({ id: parseInt(id, 10), ...plant });
+    return editedPlant;
+  }
+
+  public async removeById(id: string): Promise<void> {
+    const isPlantRemoved = await this.model.removeById(id);
+    if (!isPlantRemoved) throw new NotFoundException('Plant not Found!');
   }
 }
 
