9432fd9e0206f2575fbb6e4502c02b7cc972d898
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-04 14:37:21 -0300

    ✨ feat: exercise 11

--- 4-ciencia-da-computacao/1-introducao-a-python/dia-1-aprendendo-python/exercise-2.py
+++ 4-ciencia-da-computacao/1-introducao-a-python/dia-1-aprendendo-python/exercise-2.py
@@ -75,3 +75,37 @@ print(info)
 
 info.pop("origem")
 print(info)
+
+# Range
+
+# vamos converter o range em uma lista para ajudar na visualização
+
+# definimos somente o valor de parada
+list(range(5))  # saída: [0, 1, 2, 3, 4]
+
+# definimos o valor inicial e o de parada
+list(range(1, 6))  # saída: [1, 2, 3, 4, 5]
+
+# definimos valor inicial, de parada e modificamos o passo para 2
+list(range(1, 11, 2))  # saída: [1, 3, 5, 7, 9]
+
+# podemos utilizar valores negativos para as entradas também
+list(range(10, 0, -1))  # saída: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+my_array = [20, 20, 1, 2]
+
+freq_dict = {}
+
+for item in my_array:
+    if (item in freq_dict):
+        freq_dict[item] += 1
+    else:
+        freq_dict[item] = 1
+
+for key, valor in freq_dict.items():
+    print(f"{key} : {valor}")
+
+# Saída
+20: 2
+1: 1
+2: 1
