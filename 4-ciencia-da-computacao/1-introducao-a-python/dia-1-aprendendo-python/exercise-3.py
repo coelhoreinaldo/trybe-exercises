3df4cc8f9cf55ebb382344b8fe4726105f02fbab
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-04 15:28:38 -0300

    ✨ feat: ex 13, for in

--- 4-ciencia-da-computacao/1-introducao-a-python/dia-1-aprendendo-python/exercise-3.py
+++ 4-ciencia-da-computacao/1-introducao-a-python/dia-1-aprendendo-python/exercise-3.py
@@ -1,3 +1,5 @@
+# exercise-12
+
 # number = 5;
 # counter = 1;
 # result = 1;
@@ -14,4 +16,14 @@ result = 1
 # Usamos number + 1 pro range ir até o number
 for factor in range(1, number+1):
     result *= factor
-print(result)
+# print(result)
+
+# exercise-13
+
+ratings = [6, 8, 5, 9, 10]
+new_ratings = []
+
+for rating in ratings:
+    new_ratings.append(rating * 10)
+
+print(new_ratings)
