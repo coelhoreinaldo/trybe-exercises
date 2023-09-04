e3241512e9b76bc84a93a3380fe1e3c89d14ad83
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-04 14:30:55 -0300

    ✨ feat: dict

--- 4-ciencia-da-computacao/1-introducao-a-python/dia-1-aprendendo-python/exercise-2.py
+++ 4-ciencia-da-computacao/1-introducao-a-python/dia-1-aprendendo-python/exercise-2.py
@@ -35,3 +35,27 @@ var = set();
 var.add('Reinaldo Coelho')
 
 print(var)
+
+# Frozen Set
+
+permissions = frozenset(["member", "group"])  # assim como o set, qualquer estrutura iterável pode ser utilizada para criar um frozenset
+
+permissions.union({"user"})  # novos conjuntos imutáveis podem ser criados à partir do original, mas o mesmo não pode ser modificado
+
+permissions.intersection({"user", "member"})  # retorna um conjunto resultante da intersecção dos conjuntos
+
+permissions.difference({"user"})  # retorna a diferença entre os dois conjuntos
+
+# Dict
+
+people_by_id = {1: "Maria", 2: "Fernanda", 3: "Felipe"}  # elementos no formato "chave: valor" separados por vírgula, envolvidos por chaves
+
+people_by_name = {"Maria": 1, "Fernanda": 2, "Felipe": 3}  # outro exemplo, dessa vez usando strings como chaves. As aspas são necessárias para que o Python não ache que `Maria`, `Fernanda` e `Felipe` sejam variáveis.
+
+# elementos são acessados por suas chaves
+people_by_id[1]  # saída: Maria
+
+# elementos podem ser removidos com a palavra chave del
+del people_by_id[1]
+people_by_id.items()  # dict_items([(2, "Fernanda"), (3, "Felipe")])
+# é retornada uma coleção iterável de tuplas contendo chaves e valores
