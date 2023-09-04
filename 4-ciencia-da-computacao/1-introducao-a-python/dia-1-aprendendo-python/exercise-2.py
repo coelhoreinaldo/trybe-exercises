89c1150b91bf7464a6925958c478c7ea60499c03
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-04 14:26:07 -0300

    ✨ feat: python exercise 7

--- 4-ciencia-da-computacao/1-introducao-a-python/dia-1-aprendendo-python/exercise-2.py
+++ 4-ciencia-da-computacao/1-introducao-a-python/dia-1-aprendendo-python/exercise-2.py
@@ -3,3 +3,9 @@ trybe_course = ["Introdução", "Front-end", "Back-end"]
 # exercise-5 
 
 trybe_course.append('Ciência da Computação')
+
+# exercise-6
+
+trybe_course[0] = 'Fundamentos'
+
+print(trybe_course)
