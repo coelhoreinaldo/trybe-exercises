377c5c7a926ce0088e283d8b6a86bda754a322f5
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-10 09:58:57 -0300

    ✨ feat: open close

--- 4-ciencia-da-computacao/1-introducao-a-python/dia-2-entrada-e-saida-de-dados-com-testes/example-2/file.py
+++ 4-ciencia-da-computacao/1-introducao-a-python/dia-2-entrada-e-saida-de-dados-com-testes/example-2/file.py
@@ -1,5 +1,11 @@
 with open("arquivo.txt", "w") as file:
     file.write("Maria 45\n")
     file.write("Miguel 33\n")
+    print("Túlio 22", file=file)
+    LINES = ["Alberto 35\n", "Betina 22\n", "João 42\n", "Victor 19\n"]
+    file.writelines(LINES)
 
-print(file)
+
+with open("arquivo.txt", "r") as file:
+    for line in file:
+        print(line)
