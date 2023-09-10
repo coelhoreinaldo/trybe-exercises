f9fdbd08a78a78ac046fd9d4336b32fd16f6f416
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-10 12:12:34 -0300

    ✨ feat: exercises

---
+++ 4-ciencia-da-computacao/1-introducao-a-python/dia-2-entrada-e-saida-de-dados-com-testes/exercises/exercise-1.py
@@ -0,0 +1,9 @@
+def vertical_inverted_ladder(word):
+    for removed_letters in range(len(word)):
+        for index in range(len(word) - removed_letters):
+            print(word[index], end="")
+        print()
+
+
+name = input("Digite um nome: ")
+vertical_inverted_ladder(name)
