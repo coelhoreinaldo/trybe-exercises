226b955feb1195e58709c088148d1ba61cd4a3dc
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-12 10:57:25 -0300

    ✨ feat: practice 1

---
+++ 4-ciencia-da-computacao/2-algoritmos/dia-2-complexidade-de-algoritmos/practice_1.py
@@ -0,0 +1,12 @@
+def contains_duplicate(numbers):
+    numbers.sort()
+    previous_number = "not a number"
+    for number in numbers:
+        if previous_number == number:
+            return True
+        previous_number = number
+
+    return False
+
+
+# O(n log n)
