de29862fc7a8375df7390c67267272a4fea18d33
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-14 10:28:25 -0300

    ✨ feat: count even recursive

--- 4-ciencia-da-computacao/2-algoritmos/dia-2-recursividade-e-estrategias-para-solucao-de-problemas/practice_exercises/exercise_1.py
+++ 4-ciencia-da-computacao/2-algoritmos/dia-2-recursividade-e-estrategias-para-solucao-de-problemas/practice_exercises/exercise_1.py
@@ -1,14 +1,23 @@
 # contar numero par
 
 
+# def count_even(n):
+#     count = 0
+#     for number in range(1, n + 1):
+#         if number % 2 == 0:
+#             count += 1
+#     print(count)
+#     return count
+
+
 def count_even(n):
-    count = 0
-    for number in range(1, n + 1):
-        if number % 2 == 0:
-            count += 1
-    print(count)
-    return count
+    if n <= 1:
+        return 0
+    elif n % 2 == 0:
+        return 1 + count_even(n - 1)
+    else:
+        return count_even(n - 1)
 
 
 if __name__ == "__main__":
-    count_even(6)
+    print(count_even(8))
