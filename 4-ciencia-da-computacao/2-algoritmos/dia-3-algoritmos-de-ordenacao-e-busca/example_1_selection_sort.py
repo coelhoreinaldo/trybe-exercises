a05b9c682c1e65d5cfc22ff908b64588935e7cf9
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-15 09:08:34 -0300

    ✨ feat: selection sort

--- 4-ciencia-da-computacao/2-algoritmos/dia-3-algoritmos-de-ordenacao-e-busca/example_1_selection_sort.py
+++ 4-ciencia-da-computacao/2-algoritmos/dia-3-algoritmos-de-ordenacao-e-busca/example_1_selection_sort.py
@@ -26,3 +26,30 @@ numbers = [7, 5, 9, 2, 6, 8]
 print(f"Lista inicial: {numbers}")
 ordered_numbers = selection_sort(numbers)
 print(f"Lista final: {ordered_numbers}")
+
+
+def search(numbers, start, end):
+    min_element = numbers[start]
+    min_element_index = start
+
+    for i in range(start + 1, end):  # Busca pelo menor elemento
+        if numbers[i] < min_element:
+            min_element = numbers[i]
+            min_element_index = i
+
+    return min_element_index  # Retorna a posição do menor elemento
+
+
+def selection_sort(numbers):
+    n = len(numbers)
+
+    for index in range(
+        n - 1
+    ):  # Início da iteração para ordenar os N-1 elementos
+        min_element_index = search(numbers, index, n)
+        numbers[index], numbers[min_element_index] = (
+            numbers[min_element_index],
+            numbers[index],
+        )  # Trocando os elementos utilizando desempacotamento.
+
+    return numbers
