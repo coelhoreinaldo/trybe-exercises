562fc986f1c31b9493b9367a400c20fc21501048
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-20 10:51:25 -0300

    ✨ feat: setters and getters

--- 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-1-poo-em-python/examples/LiquidificadorGetsSets.py
+++ 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-1-poo-em-python/examples/LiquidificadorGetsSets.py
@@ -1,27 +1,28 @@
 class Liquidificador:
-    def get_cor(self):
+    @property
+    def cor(self):
         return self.__cor.upper()
 
-    def set_cor(self, nova_cor):
+    @cor.setter
+    def cor(self, nova_cor):
         if nova_cor.lower() == "turquesa":
             raise ValueError("Não existe liquidificador turquesa")
 
         self.__cor = nova_cor
 
     def __init__(self, cor, potencia, tensao, preco):
-        # Observe que usamos o setter para já validarmos o primeiro valor
-        self.set_cor(cor)
+        # Observe que usamos o setter para já validarmos o primeiro valor:
+        # usamos self.cor, que chama o setter, e não self.__cor que manipula
+        # o atributo diretamente
+        self.cor = cor
 
         # Demais variáveis omitidas para este exemplo
 
 
 liquidificador = Liquidificador("Azul", "110", "127", "200")
 
-# print(f"A cor atual é {liquidificador.__cor}")
-# AttributeError: 'Liquidificador' object has no attribute '__cor'
+liquidificador = Liquidificador("Rosa", "110", "127", "200")
 
-print(f"A cor atual é {liquidificador.get_cor()}")
-# A cor atual é AZUL
-liquidificador.set_cor("Preto")
-print(f"Após pintarmos, a nova cor é {liquidificador.get_cor()}")
-# Após pintarmos, a nova cor é PRETO
+print(liquidificador.cor)  # ROSA
+liquidificador.cor = "Vermelho"
+print(liquidificador.cor)  # VERMELHO
