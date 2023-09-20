4fdfa04d386c4fe9f27defcc603aabeebbb20fbb
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-20 10:39:57 -0300

    ✨ feat: class liquidificador methods

--- 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-1-poo-em-python/examples/Liquidificador.py
+++ 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-1-poo-em-python/examples/Liquidificador.py
@@ -9,6 +9,25 @@ class Liquidificador:
         self.__velocidade_maxima = 3
         self.__corrente_atual_no_motor = 0
 
+    def ligar(self, velocidade):
+        if velocidade > self.__velocidade_maxima or velocidade < 0:
+            raise ValueError(
+                f"Velocidade deve estar entre 0 e {self.__velocidade_maxima}"
+            )
+
+        self.__velocidade = velocidade
+        self.__corrente_atual_no_motor = (
+            (self._potencia / self._tensao) / self.__velocidade_maxima
+        ) * velocidade
+        self.__ligado = True
+
+    def desligar(self):
+        self.__ligado = False
+        self.__velocidade = 0
+
+    def esta_ligado(self):
+        return self.__ligado
+
 
 meu_liquidificador = Liquidificador("Azul", 200, 127, 200)
 seu_liquidificador = Liquidificador(
