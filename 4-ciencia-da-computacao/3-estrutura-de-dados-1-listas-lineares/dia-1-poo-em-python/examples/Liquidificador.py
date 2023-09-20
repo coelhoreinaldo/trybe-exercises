b359520fd534174cba694fe5a80905d9522422e9
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-20 10:59:23 -0300

    ✨ feat: pessoa class

--- 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-1-poo-em-python/examples/Liquidificador.py
+++ 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-1-poo-em-python/examples/Liquidificador.py
@@ -1,13 +1,24 @@
 class Liquidificador:
     def __init__(self, cor, potencia, tensao, preco):
         self.preco = preco
-        self.cor = cor
+        self.__cor = cor
         self._potencia = potencia
         self._tensao = tensao
         self.__ligado = False
         self.__velocidade = 0
         self.__velocidade_maxima = 3
         self.__corrente_atual_no_motor = 0
+
+    @property
+    def cor(self):
+        return self.__cor.upper()
+
+    @cor.setter
+    def cor(self, nova_cor):
+        if nova_cor.lower() == "turquesa":
+            raise ValueError("Não existe liquidificador turquesa")
+
+        self.__cor = nova_cor
 
     def ligar(self, velocidade):
         if velocidade > self.__velocidade_maxima or velocidade < 0:
@@ -29,11 +40,22 @@ class Liquidificador:
         return self.__ligado
 
 
-liquidificador_azul = Liquidificador("Azul", 200, 127, 200)
-liquidificador_vermelho = Liquidificador(
-    cor="Vermelho", potencia=250, tensao=220, preco=100
-)
-liquidificador_vermelho.ligar(1)
-print("Está ligado?", liquidificador_vermelho.esta_ligado())
-liquidificador_vermelho.desligar()
-print("Está ligado?", liquidificador_vermelho.esta_ligado())
+liquidificador = Liquidificador("Rosa", 110, 127, 200)
+liquidificador.cor = "Azul"
+
+
+class Pessoa:
+    def __init__(self, nome, saldo_na_conta):
+        self.nome = nome
+        self.saldo_na_conta = saldo_na_conta
+        self.liquidificador = None
+
+    def comprar_liquidificador(self, liquidificador):
+        if liquidificador.preco <= self.saldo_na_conta:
+            self.saldo_na_conta -= liquidificador.preco
+            self.liquidificador = liquidificador
+
+
+pessoa_cozinheira = Pessoa("Paola Carosella", 1000)
+pessoa_cozinheira.comprar_liquidificador(liquidificador)
+print(pessoa_cozinheira.liquidificador.cor)
