eabab592ff338c9a3d946fc5701731ce7e896764
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-20 14:43:20 -0300

    ✨ feat: to fix exercise

--- 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-1-poo-em-python/practice/Liquidificador.py
+++ 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-1-poo-em-python/practice/Liquidificador.py
@@ -1,3 +1,6 @@
+from Ventilador import Ventilador
+
+
 class Liquidificador:
     def __init__(self, cor, potencia, tensao, preco):
         self.preco = preco
@@ -49,18 +52,33 @@ class Pessoa:
         self.nome = nome
         self.saldo_na_conta = saldo_na_conta
         self.liquidificador = None
+        self.ventilador = None
 
     def comprar_liquidificador(self, liquidificador):
         if liquidificador.preco <= self.saldo_na_conta:
             self.saldo_na_conta -= liquidificador.preco
             self.liquidificador = liquidificador
 
+    def comprar_ventilador(self, ventilador):
+        if ventilador.preco <= self.saldo_na_conta:
+            self.saldo_na_conta -= ventilador.preco
+            self.ventilador = ventilador
+
     def __str__(self) -> str:
-        return (
-            f"{self.nome} - possui {self.saldo_na_conta} reais em sua conta."
-        )
+        message = f"""
+            - {self.nome} - possui {self.saldo_na_conta} reais em sua conta.
+            """
+        if self.ventilador:
+            message += f"- Possui um ventilador ${self.ventilador.cor}"
+        else:
+            message += "Não possui um ventilador"
+
+        return message
 
 
 pessoa_cozinheira = Pessoa("Paola Carosella", 1000)
 pessoa_cozinheira.comprar_liquidificador(liquidificador)
-print(pessoa_cozinheira.liquidificador.cor)
+
+ventilador = Ventilador(100, "Preto", 100, 127)
+pessoa_cozinheira.comprar_ventilador(ventilador)
+print(pessoa_cozinheira)
