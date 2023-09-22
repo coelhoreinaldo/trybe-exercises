7885cd413041b4f67c3c29ea5696e2b1eaa4b07e
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-22 09:46:10 -0300

    ✨ feat: memory

--- 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-2-arrays/examples/array_example.py
+++ 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-2-arrays/examples/array_example.py
@@ -1,6 +1,4 @@
-"""Perceba que temos uma coleção de valores
-e operações que atuam sobre estes valores,
-de acordo com o que foi definido pelo TAD."""
+import sys
 
 
 class ListaArray:
@@ -21,17 +19,30 @@ class ListaArray:
 
 
 array = ListaArray()
-array.set(0, "Felipe")
-array.set(1, "Ana")
-array.set(2, "Shirley")
-array.set(3, "Miguel")
 
-print(array.get(0))  # Felipe
-print(array.get(2))  # Shirley
-print("------")
+# index = 0
 
-index = 0
+# while index < len(array):
+#     print("Index:", index, ", Nome:", array.get(index))
+#     index += 1
 
-while index < len(array):
-    print("Index:", index, ", Nome:", array.get(index))
-    index += 1
+# ...
+
+array.set(0, "Marcos")
+array.set(1, "Patrícia")
+
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)
+
+array.set(2, "Matheus")
+array.set(3, "Giovana")
+
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)
+
+array.set(4, "Alberto")
+array.set(5, "Marta")
+array.set(6, "Túlio")
+array.set(7, "Michelle")
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)  # 120
