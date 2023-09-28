322979503d875287f01684c2a21252b38a631835
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-28 16:11:49 -0300

    ✨ feat: insert_first and insert_last

--- 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-3-nos-e-listas-encadeadas/examples/linked_list_content.py
+++ 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-3-nos-e-listas-encadeadas/examples/linked_list_content.py
@@ -11,3 +11,21 @@ class LinkedList:
 
     def __len__(self) -> int:
         return self.__length
+
+    def insert_first(self, value):
+        first_value = Node(value)
+        first_value.next = self.head_value
+        self.head_value = first_value
+        self.__length += 1
+
+    def insert_last(self, value):
+        last_value = Node(value)
+        current_value = self.head_value
+
+        if current_value is None:
+            return self.insert_first(value)
+
+        while current_value.next:
+            current_value = current_value.next
+        current_value.next = last_value
+        self.__length += 1
