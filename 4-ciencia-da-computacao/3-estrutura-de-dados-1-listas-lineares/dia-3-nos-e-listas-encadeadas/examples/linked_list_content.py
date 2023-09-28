b8dff4300afee4be2ac2f876c0b5e44636e01975
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-28 16:39:50 -0300

    ✨ feat: get_element_at

--- 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-3-nos-e-listas-encadeadas/examples/linked_list_content.py
+++ 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-3-nos-e-listas-encadeadas/examples/linked_list_content.py
@@ -84,3 +84,14 @@ class LinkedList:
         value_to_be_removed.next = None
         self.__length -= 1
         return value_to_be_removed
+
+    def get_element_at(self, position):
+        value_returned = None
+        value_to_be_returned = self.head_value
+        if value_to_be_returned:
+            while position > 0 and value_to_be_returned.next:
+                value_to_be_returned = value_to_be_returned.next
+                position -= 1
+            if value_to_be_returned:
+                value_returned = Node(value_to_be_returned.value)
+        return value_returned
