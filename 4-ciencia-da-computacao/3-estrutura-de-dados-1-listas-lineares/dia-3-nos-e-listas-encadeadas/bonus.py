7d1c15e37209d7bbdb39c680e21ef0dfda53b110
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-29 11:29:48 -0300

    ✨ feat: bonus, remove duplicates

--- 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-3-nos-e-listas-encadeadas/bonus.py
+++ 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-3-nos-e-listas-encadeadas/bonus.py
@@ -10,3 +10,16 @@ def remove_duplicates(linkedList: LinkedList):
             list_with_unique_elements.insert_last(current_node.value)
 
     return list_with_unique_elements
+
+
+list_with_duplicate_elements = LinkedList()
+list_with_duplicate_elements.insert_first(1)
+list_with_duplicate_elements.insert_first(1)
+list_with_duplicate_elements.insert_first(2)
+list_with_duplicate_elements.insert_first(3)
+list_with_duplicate_elements.insert_first(4)
+list_with_duplicate_elements.insert_first(4)
+list_with_duplicate_elements.insert_first(3)
+
+print(list_with_duplicate_elements)
+print(remove_duplicates(list_with_duplicate_elements))
