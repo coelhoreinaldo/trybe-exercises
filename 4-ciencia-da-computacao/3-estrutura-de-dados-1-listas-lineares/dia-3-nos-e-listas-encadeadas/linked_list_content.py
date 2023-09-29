dadb0e68c1968f108ca8ef5e9e73397d2c445d41
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-29 10:50:27 -0300

    ✨ feat: get node at

--- 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-3-nos-e-listas-encadeadas/linked_list_content.py
+++ 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-3-nos-e-listas-encadeadas/linked_list_content.py
@@ -103,6 +103,14 @@ class LinkedList:
         self.head_value = None
         self.__length = 0
 
+    def __get_node_at(self, position):
+        value_to_be_returned = self.head_value
+        if value_to_be_returned:
+            while position > 0 and value_to_be_returned.next:
+                value_to_be_returned = value_to_be_returned.next
+                position -= 1
+        return value_to_be_returned
+
 
 if __name__ == "__main__":
     linked_list = LinkedList()
