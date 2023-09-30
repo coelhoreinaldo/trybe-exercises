92cf167ac2f378d0c57ce6476335499f0f463173
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-30 10:56:07 -0300

    ✨ feat: create Stack class

---
+++ 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-4-pilhas-e-filas/stack.py
@@ -0,0 +1,19 @@
+class Stack:
+    def __init__(self) -> None:
+        self._data = list()
+
+    def size(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return not bool(self.size())
+
+    def push(self, value):
+        self._data.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        value = self._data[-1]
+        del self._data[-1]
+        return value
