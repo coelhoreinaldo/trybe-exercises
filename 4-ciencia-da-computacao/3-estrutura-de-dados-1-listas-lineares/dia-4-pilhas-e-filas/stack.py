ea703e5b331e03c40d42d28eb629bb06d35a72f5
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-30 11:02:28 -0300

    ✨ feat: peek, clear, str

--- 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-4-pilhas-e-filas/stack.py
+++ 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-4-pilhas-e-filas/stack.py
@@ -17,3 +17,50 @@ class Stack:
         value = self._data[-1]
         del self._data[-1]
         return value
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        value = self._data[-1]
+        return value
+
+    def clear(self):
+        self._data.clear()
+
+    def __str__(self) -> str:
+        str_items = ""
+        for i in range(self.size()):
+            value = self._data[i]
+            str_items += str(value)
+            if i + 1 < self.size():
+                str_items += ", "
+
+        return "Stack(" + str_items + ")"
+
+
+if __name__ == "__main__":
+    elements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    content_stack = Stack()
+
+    for elem in elements:
+        content_stack.push(elem)
+    # saída: Stack(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    print(content_stack)
+    # saída: 10
+    print(content_stack.size())
+
+    # saída: 10
+    print(content_stack.peek())
+    # saída: 10, pois a função retorna o elemento que está sendo retirado
+    print(content_stack.pop())
+
+    # saída: 9, pois, após o 10 ter sido removido, o 9 se tornou o
+    # elemento do topo da pilha
+    print(content_stack.peek())
+    # saída: 9
+    print(content_stack.size())
+
+    # saída: None, pois a função não retorna nada!
+    print(content_stack.clear())
+    # saída: 0
+    print(content_stack.size())
