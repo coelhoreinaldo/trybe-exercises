d6c20248e2780fde98ac9c13b0cda34571f80aae
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-01 08:51:16 -0300

    ✨ feat: queue and enqueue

--- 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-4-pilhas-e-filas/queue_1.py
+++ 4-ciencia-da-computacao/3-estrutura-de-dados-1-listas-lineares/dia-4-pilhas-e-filas/queue_1.py
@@ -1,12 +1,45 @@
 class Queue:
     def __init__(self):
-        self.queue = list()
+        self._data = list()
 
     def enqueue(self, value):
-        self.queue.append(value)
+        self._data.append(value)
 
     def dequeue(self):
-        if len(self.queue) == 0:
+        if len(self._data) == 0:
             return None
 
-        return self.queue.pop(0)
+        return self._data.pop(0)
+
+    def __str__(self) -> str:
+        str_items = ""
+        for i in range(len(self._data)):
+            value = self._data[i]
+            str_items += str(value)
+            if i + 1 < len(self._data):
+                str_items += ", "
+
+        return "Queue(" + str_items + ")"
+
+
+if __name__ == "__main__":
+    elements = ["Milkshake", "Batata Frita", "Refrigerante"]
+    content_queue = Queue()
+
+    print(content_queue)
+    # Saída: Queue()
+
+    for elem in elements:
+        content_queue.enqueue(elem)
+
+    print(content_queue)
+    # Saída: Queue(Milkshake, Batata Frita, Refrigerante)
+
+    content_queue.dequeue()
+    print(content_queue)
+    # Saída: Queue(Batata Frita, Refrigerante)
+
+    print(content_queue.dequeue())
+    # Saída: Batata Frita
+    # porque "Milkshake" já foi removido e "Batata Frita" se
+    # tornou o primeiro elemento da fila
