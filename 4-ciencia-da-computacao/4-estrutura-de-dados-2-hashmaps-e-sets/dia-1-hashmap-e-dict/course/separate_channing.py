80cda94043c65fde589401948642b945d77d3722
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-03 15:22:28 -0300

    ✨ feat: separate channing

---
+++ 4-ciencia-da-computacao/4-estrutura-de-dados-2-hashmaps-e-sets/dia-1-hashmap-e-dict/course/separate_channing.py
@@ -0,0 +1,37 @@
+class Employee:
+    def __init__(self, id_num, name):
+        self.id_num = id_num
+        self.name = name
+
+    def __str__(self) -> str:
+        return f"(id: {self.id_num}, name: {self.name})"
+
+    def __repr__(self) -> str:
+        return f"(id: {self.id_num}, name: {self.name})"
+
+
+class HashMap:
+    def __init__(self):
+        self._buckets = [[] for i in range(10)]
+
+    def get_address(self, id_num):
+        return id_num % 10
+
+    def insert(self, employee):
+        address = self.get_address(employee.id_num)
+        self._buckets[address].append(employee)
+
+    def get_value(self, id_num):
+        address = self.get_address(id_num)
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return item.name
+        return None
+
+    # def has(self, id_num):
+    #     address = self.get_address(id_num)
+    #     return self._buckets[address] is not None
+
+    # def update_value(self, id_num, new_name):
+    #     address = self.get_address(id_num)
+    #     self._buckets[address].name = new_name
