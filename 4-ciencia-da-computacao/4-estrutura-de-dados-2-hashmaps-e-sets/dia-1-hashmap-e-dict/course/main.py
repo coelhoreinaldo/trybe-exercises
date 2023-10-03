1e0ca88f6b05e4e50aad326bfcdb9032df4804a3
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-03 09:38:02 -0300

    ✨ feat: exercise 1

--- 4-ciencia-da-computacao/4-estrutura-de-dados-2-hashmaps-e-sets/dia-1-hashmap-e-dict/course/main.py
+++ 4-ciencia-da-computacao/4-estrutura-de-dados-2-hashmaps-e-sets/dia-1-hashmap-e-dict/course/main.py
@@ -3,6 +3,12 @@ class Employee:
         self.id_num = id_num
         self.name = name
 
+    def __str__(self) -> str:
+        return f"(id: {self.id_num}, name: {self.name})"
+
+    def __repr__(self) -> str:
+        return f"(id: {self.id_num}, name: {self.name})"
+
 
 class HashMap:
     def __init__(self):
@@ -22,3 +28,14 @@ class HashMap:
     def has(self, id_num):
         address = self.get_address(id_num)
         return self._buckets[address] is not None
+
+
+employees = [(14, "name1"), (23, "name2"), (10, "name3"), (9, "name4")]
+
+hashmap = HashMap()
+
+for e_id, e_name in employees:
+    curr_employee = Employee(e_id, e_name)
+    hashmap.insert(curr_employee)
+
+print(hashmap.get_value(23))
