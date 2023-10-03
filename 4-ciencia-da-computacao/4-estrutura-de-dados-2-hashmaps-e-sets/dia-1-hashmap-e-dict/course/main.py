6c33fac7c17845daee239a79575527c341e15392
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-03 09:30:55 -0300

    ✨ feat: get

--- 4-ciencia-da-computacao/4-estrutura-de-dados-2-hashmaps-e-sets/dia-1-hashmap-e-dict/course/main.py
+++ 4-ciencia-da-computacao/4-estrutura-de-dados-2-hashmaps-e-sets/dia-1-hashmap-e-dict/course/main.py
@@ -14,3 +14,11 @@ class HashMap:
     def insert(self, employee):
         address = self.get_address(employee.id_num)
         self._buckets[address] = employee
+
+    def get_value(self, id_num):
+        address = self.get_address(id_num)
+        return self._buckets[address].name
+
+    def has(self, id_num):
+        address = self.get_address(id_num)
+        return self._buckets[address] is not None
