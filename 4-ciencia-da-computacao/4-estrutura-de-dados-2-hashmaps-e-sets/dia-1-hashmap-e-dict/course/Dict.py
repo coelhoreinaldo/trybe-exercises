94b168d25443570219e3846b82ffa10b16b57186
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-04 08:00:44 -0300

    ✨ feat: dict

---
+++ 4-ciencia-da-computacao/4-estrutura-de-dados-2-hashmaps-e-sets/dia-1-hashmap-e-dict/course/Dict.py
@@ -0,0 +1,12 @@
+employee_registry = {}
+
+employee_registry[14] = "name1"
+employee_registry[23] = "name2"
+employee_registry[10] = "name3"
+employee_registry[9] = "name4"
+
+print(employee_registry)
+
+employee_registry[10] = "name30"
+
+print(f"Novo valor do id 10, após a atualização: {employee_registry[10]}")
