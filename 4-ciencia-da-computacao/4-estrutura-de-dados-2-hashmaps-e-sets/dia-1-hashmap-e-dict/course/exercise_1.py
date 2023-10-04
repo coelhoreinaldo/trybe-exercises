026b488893374c39647fe9a3eeadd41574f7603c
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-04 10:27:14 -0300

    ✨ feat: cache

--- 4-ciencia-da-computacao/4-estrutura-de-dados-2-hashmaps-e-sets/dia-1-hashmap-e-dict/course/exercise_1.py
+++ 4-ciencia-da-computacao/4-estrutura-de-dados-2-hashmaps-e-sets/dia-1-hashmap-e-dict/course/exercise_1.py
@@ -1,38 +1,41 @@
 class Hierarchy:
-    def __init__(self, subordinates):
-        self.subordinates = subordinates
+    def __init__(self, k):
+        self.subordinates = {}
         self.scores = {}
+        self.k = k
+
+    def add_person(self, boss, person):
+        if not boss:
+            self.subordinates[person] = []
+            self.scores[person] = 1
+            return
+
+        self.scores[boss] += 1
+
+        if len(self.subordinates[boss]) < self.k:
+            self.subordinates[boss].append(person)
+            self.subordinates[person] = []
+            self.scores[person] = 1
+        else:
+            self.add_person(self.subordinates[boss][0], person)
 
     def get_score(self, person):
-        if person in self.scores:
-            print("Já calculei esse score antes")
-            return self.scores[person]
-
-        this_score = 1
-
-        for subordinate in self.subordinates[person]:
-            this_score += self.get_score(subordinate)
-        self.scores[person] = this_score
-
-        return this_score
+        return self.scores[person]
 
 
 if __name__ == "__main__":
-    subordinates = {
-        1: [2, 3],
-        2: [4],
-        3: [],
-        4: [5, 6],
-        5: [7],
-        6: [],
-        7: [],
-    }
+    hierarchy = Hierarchy(2)
+    hierarchy.add_person(None, 1)
+    hierarchy.add_person(1, 2)
+    hierarchy.add_person(1, 3)
+    hierarchy.add_person(2, 4)
+    hierarchy.add_person(4, 5)
+    hierarchy.add_person(4, 6)
+    hierarchy.add_person(5, 7)
+    print("\nAntes das novas inserções")
+    print(f"Subordinates: {hierarchy.subordinates}")
 
-    hierarchy = Hierarchy(subordinates)
-    print(hierarchy.get_score(1))
-    print(hierarchy.get_score(2))
-    print(hierarchy.get_score(3))
-    print(hierarchy.get_score(4))
-    print(hierarchy.get_score(5))
-    print(hierarchy.get_score(6))
-    print(hierarchy.get_score(7))
+    hierarchy.add_person(4, 8)
+    hierarchy.add_person(4, 9)
+    print("\nApós novas inserções")
+    print(f"Subordinates: {hierarchy.subordinates}\n")
