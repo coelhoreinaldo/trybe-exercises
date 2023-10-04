a647552d29da2347eef7104df48f44fbe1db6c5f
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-04 08:03:23 -0300

    ✨ feat: dict

--- 4-ciencia-da-computacao/4-estrutura-de-dados-2-hashmaps-e-sets/dia-1-hashmap-e-dict/course/Dict.py
+++ 4-ciencia-da-computacao/4-estrutura-de-dados-2-hashmaps-e-sets/dia-1-hashmap-e-dict/course/Dict.py
@@ -10,3 +10,19 @@ print(employee_registry)
 employee_registry[10] = "name30"
 
 print(f"Novo valor do id 10, após a atualização: {employee_registry[10]}")
+
+dict1 = {}
+dict2 = dict()
+
+
+dict3 = {1: "name1", 2: "name2"}
+print(f"Dicionário 1: {dict1}")
+print(f"Dicionário 2: {dict2}")
+print(f"Dicionário 3: {dict3}")
+
+dict1[14] = "name1"
+print(f"Novo dicionário 1, pós inserção/alteração: {dict1}")
+
+name = dict1[14]
+del dict1[14]
+print(f"Dicionário 1 pós consulta e deleção: {dict1}")
