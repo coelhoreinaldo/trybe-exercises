4d5d5eeb9c56da5fe3a9dd4f211f8339193c8b7e
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-05 09:00:46 -0300

    ✨ feat: exercise 2, create str method

--- 4-ciencia-da-computacao/4-estrutura-de-dados-2-hashmaps-e-sets/dia-2-set/course/exercise_1.py
+++ 4-ciencia-da-computacao/4-estrutura-de-dados-2-hashmaps-e-sets/dia-2-set/course/exercise_1.py
@@ -3,6 +3,17 @@ class Conjunto:
         self.set = [False] * 1001
         self.last_element = 0
 
+    def __str__(self) -> str:
+        string = "{"
+
+        for index, is_in_set in enumerate(self.set):
+            if is_in_set:
+                string += str(index)
+                if index < self.last_element:
+                    string += ", "
+        string += "}"
+        return string
+
     def add(self, item):
         if not self.set[item]:
             self.set[item] = True
@@ -16,4 +27,4 @@ if __name__ == "__main__":
     for item in [0, 10, 100, 1000]:
         collection.add(item)
 
-    print(collection.set)
+    print(collection)
